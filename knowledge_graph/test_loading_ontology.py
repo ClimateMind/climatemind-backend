80ed8ef0252763347ed8d18ab6cae6b25400dc71
tom deadman <tom.deadman@river-island.com>
2020-07-12 23:05:21 +0100

    added support for multiple queries (should be returned in a key-value structure), further encapuslated the ontology code, added multiple configs for later mocking of Mind and testing, started testing of API <tom>

--- knowledge_graph/test_loading_ontology.py
+++ knowledge_graph/test_loading_ontology.py
@@ -3,11 +3,6 @@ from .mind import Mind
 
 
 class TestLoadingOntology(unittest.TestCase):
-    def test_invalid_ontology_source(self):
-        onto = Mind("bad")
-
-        self.assertIsNone(onto.get_ontology())
-
     def test_valid_ontology_source(self):
-        onto = Mind("./climate_mind_ontology")
+        onto = Mind()
         self.assertIsNotNone(onto.get_ontology())
