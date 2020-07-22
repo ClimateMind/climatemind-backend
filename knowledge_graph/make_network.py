c6fee414beadb948239ee9fa4b04c8eb2dfffda4
Sean Payne <sean.payne.es@gmail.com>
2020-07-22 23:45:43 +0900

    updated network code

--- knowledge_graph/make_network.py
+++ knowledge_graph/make_network.py
@@ -3,15 +3,14 @@ import pandas as pd
 from network_class import Network
 from owlready2 import *
 
-def search_node(ontology, start_point):
-    ont_start_point = ontology.search_one(label=start_point)
-    node_network = Network(ontology, "causes_or_promotes", ont_start_point)
+def get_edges(ontology):
+    node_network = Network(ontology)
     node_network.dfs_labeled_edges()
     return node_network.get_results()
 
 
 def test_answer():
-    assert search_node(get_ontology(onto_path).load(),"coal mining") == []
+    assert search_node(get_ontology(onto_path).load()) == []
     #need to add in the answer to this unit test.
 
 #still need to make work for when:
@@ -33,12 +32,11 @@ def main(args):
         args = args from the argument parser for the function (refNode, refOntologyPath, outputPath)
     output:
         Saves a csv file of the list of result edges (list of object, subject, predicate triples)
-    example: python3 make_network.py "coal mining" "./climate_mind_ontology20200322.owl" "output.csv"
+    example: python3 make_network.py "coal mining" "./climate_mind_ontology20200721.owl" "output.csv"
     """
     
     #set argument variables
     onto_path = args.refOntologyPath
-    target_node_label = args.refNode
     output_path = args.outputPath
     
     #load ontology
@@ -49,18 +47,19 @@ def main(args):
     [give_alias(x) for x in properties]
 
     #make list of edges along all paths leaving the target node
-    edges = search_node(onto, target_node_label)
+    edges = get_edges(onto)
 
     #save output to output Path as csv file. Later can change this to integrate well with API and front-end.
     df = pd.DataFrame([[i[0], i[1], i[2]] for i in edges],
                         columns=['subject', 'object', 'predicate'])
+    df = df.drop_duplicates() # Remove if we fix network_class
     df.to_csv(output_path, index=False)
 
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='get ontology edge from reference node')
-    parser.add_argument("refNode", type=str,
-                        help='exact node string that exists in the ontology')
+    # parser.add_argument("refNode", type=str,
+    #                     help='exact node string that exists in the ontology')
     parser.add_argument("refOntologyPath", type=str,
                         help='path to reference OWL2 ontology')
     parser.add_argument("outputPath", type=str,
