68d890427cde5b36a9e2aaaf80ca09f2b003460c
Kameron Rodrigues <kameronr@email.arizona.edu>
2020-07-24 11:46:37 -0700

    added code to save to in other graph formats
    Our data format needs to be updated to be compatible with these other formats. Should not be fixed now, but may need to be fixed later on, after we establish working API for frontend.

--- knowledge_graph/make_graph.py
+++ knowledge_graph/make_graph.py
@@ -5,7 +5,7 @@ from owlready2 import *
 import json
 import pickle
 
-# Load ontology and format into Python readable names
+# Load ontology and format object properties and annotation properties into Python readable names
 onto = get_ontology("../../Bx50aIKwEALYNmYl0CFzNp.owl").load()
 obj_properties = list(onto.object_properties())
 annot_properties = list(onto.annotation_properties())
@@ -13,7 +13,7 @@ annot_properties = list(onto.annotation_properties())
 [give_alias(x) for x in annot_properties]
 
 def convert_dataframe_to_edges():
-    edges = []    
+    edges = []
     for rows in df.itertuples():
         edge_list = [rows.subject, rows.object, rows.predicate]
         edges.append(edge_list)
@@ -23,11 +23,11 @@ def convert_dataframe_to_edges():
 def add_edges_to_graph(edges, G):
     for index in range(len(edges)):
         G.add_edge(
-                    edges[index][0], 
-                    edges[index][1], 
-                    type=edges[index][2], 
-                    properties=None
-                    )
+                   edges[index][0],
+                   edges[index][1],
+                   type=edges[index][2],
+                   properties=None
+                   )
 
 
 def add_ontology_data_to_graph_nodes(G):
@@ -37,7 +37,7 @@ def add_ontology_data_to_graph_nodes(G):
         Parameters
         ----------
         G: A networkx Graph
-    """
+        """
     for node in list(G.nodes):
         ontology_node = onto.search_one(label = node)
         class_objects = onto.get_parents_of(ontology_node)
@@ -46,23 +46,23 @@ def add_ontology_data_to_graph_nodes(G):
         G.add_nodes_from([node], individual = str(ontology_node))
         G.add_nodes_from([node], comment = str(ontology_node.comment))
         G.add_nodes_from([node], classes = set([str(parent.label[0]) for parent in class_objects if parent in onto.classes()])) #the if statement is needed to avoid the Restriction objects
-            #still don't know why Restriction Objects are in our ontology!
-            #technically each class could have multiple labels, but this way just pulling 1st label
+        #still don't know why Restriction Objects are in our ontology!
+        #technically each class could have multiple labels, but this way just pulling 1st label
         annot_properties_dict={}
         for prop in annot_properties:
             annot_properties_dict[prop] = set(eval("ontology_node."+prop))
         G.add_nodes_from([node], properties = annot_properties_dict)
-        
 
-def get_edge_properties(G):
-    """ Add edge annotation properties that exist on both nodes of an edge 
+
+def set_edge_properties(G):
+    """ Add edge annotation properties that exist on both nodes of an edge
         and create a list of properties to remove from the nodes.
         (Properties that exist on both nodes of an edge are only for the edge)
         
         Parameters
         ----------
         G: A networkx Graph
-    """
+        """
     to_remove = {}
     for edge in list(G.edges):
         node_a = edge[0]
@@ -84,38 +84,59 @@ def get_edge_properties(G):
 
 
 def remove_edge_properties_from_nodes(G, to_remove):
-    """ Remove properties form Networkx nodes that occur on both nodes of an edge 
+    """ Remove properties from Networkx nodes that occur on both nodes of an edge
         (because it marks that property is only for the edge).
         
         Parameters
         ----------
         G: A networkx graph
         to_remove: A dictionary of nodes and properties
-    """
+        """
     for item in to_remove.keys():
         node = item[0]
         prop = item[1]
         to_delete = to_remove[item]
-        G.nodes[node]["properties"][prop] = set(G.nodes[node]["properties"][prop] 
+        G.nodes[node]["properties"][prop] = set(G.nodes[node]["properties"][prop]
                                                 - set(to_delete))
 
 def save_graph_to_pickle(G):
-    with open('data.txt', 'wb') as outfile:
-        nx.write_gpickle(G, outfile)    
-                        
-                        
+    with open('Climate_Mind_DiGraph.gpickle', 'wb') as outfile:
+        nx.write_gpickle(G, outfile)
+
+def save_graph_to_gexf(G):
+    with open('Climate_Mind_DiGraph.gexf', 'wb') as outfile:
+        nx.write_gexf(G, outfile)
+
+def save_graph_to_gml(G):
+    with open('Climate_Mind_DiGraph.gml', 'wb') as outfile:
+        nx.write_gml(G, outfile)
+
+def save_graph_to_graphml(G):
+    with open('Climate_Mind_DiGraph.graphml', 'wb') as outfile:
+        nx.write_graphml(G, outfile)
+
+def save_graph_to_yaml(G):
+    with open('Climate_Mind_DiGraph.yaml', 'wb') as outfile:
+        nx.write_yaml(G, outfile)
+
+
 # Read in the triples data
 df = pd.read_csv('../../output.csv')
 
 G = nx.DiGraph()    # There should not be duplicate edges that go the same direction.
-                    # If so, need to throw an error.
-   
+# If so, need to throw an error.
+
 edges = convert_dataframe_to_edges()
 add_edges_to_graph(edges, G)
 add_ontology_data_to_graph_nodes(G)
-to_remove = get_edge_properties(G)
+to_remove = set_edge_properties(G)
 remove_edge_properties_from_nodes(G, to_remove)
+
 save_graph_to_pickle(G)
+#save_graph_to_gexf(G)
+#save_graph_to_gml(G)
+#save_graph_to_graphml(G)
+#save_graph_to_yaml(G)
 
 
 
