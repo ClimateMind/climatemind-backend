d17ebd6ec0da559697650c890ceb53b76bf68147
Kameron Rodrigues <kameronr@email.arizona.edu>
2020-07-23 20:53:31 -0700

    builds complete networkx DiGraph
    - Builds DiGraph instead of Multigraph (ontology shouldn't have duplicate edges and if it does there should be an error given to let the ontology curators know)
    - Annotates all nodes with properties, comments, and classes
    - Binds nodes to Owlready2 ontology individuals
    - Adds edge properties to edges
    - Removes edge properties from nodes as required by the ontology workaround with edge references (leaves the properties of owlready2 ontology individuals as is though for better record keeping)

--- knowledge_graph/make_graph.py
+++ knowledge_graph/make_graph.py
@@ -1,17 +1,13 @@
-import networkx as nx
-import pandas as pd
-from make_network import give_alias
-from owlready2 import *
-
-# Read in the triples data
-df = pd.read_csv('output.csv')
-
 # Load ontology and format into Python readable names
 onto = get_ontology("../../Bx50aIKwEALYNmYl0CFzNp.owl").load()
 obj_properties = list(onto.object_properties())
 annot_properties = list(onto.annotation_properties())
 [give_alias(x) for x in obj_properties]
 [give_alias(x) for x in annot_properties]
+
+
+# Read in the triples data
+df = pd.read_csv('output.csv')
 
 # Create a graph with multiple directional edges.
 graph = nx.MultiDiGraph()
@@ -53,4 +49,59 @@ def test_check_for_node(node_name):
         print(graph.nodes[node_name])
     
 test_check_for_node('human population growth')
-    
+
+
+
+
+G = nx.DiGraph() #there should not be duplicate edges that go the same direction in the ontology. if so, need to throw an error.
+edges = []
+for rows in df.itertuples():
+    my_list = [rows.subject, rows.object, rows.predicate]
+    edges.append(my_list)
+
+#add edges
+for index in range(len(edges)):
+    G.add_edge(edges[index][0],edges[index][1],type=edges[index][2], properties = None)
+
+#bind each node in the Networkx DiGraph to the associated node object from the Owlready2 ontology.
+#add all class from the Owlready2 ontology to each node of graph G
+for node in list(G.nodes):
+    ontology_node = onto.search_one(label = node)
+    class_objects = onto.get_parents_of(ontology_node)
+    annot_properties = [thing.label[0].replace(":","_") for thing in list(onto.annotation_properties())]
+    G.add_nodes_from([node], individual = ontology_node)
+    G.add_nodes_from([node], comment = ontology_node.comment)
+    G.add_nodes_from([node], classes = set([parent.label[0] for parent in class_objects if parent in onto.classes()])) #the if statement is needed to avoid the Restriction objects
+        #still don't know why Restriction Objects are in our ontology!
+        #technically each class could have multiple labels, but this way just pulling 1st label
+    annot_properties_dict={}
+    for prop in annot_properties:
+        annot_properties_dict[prop] = set(eval("ontology_node."+prop))
+    G.add_nodes_from([node], properties = annot_properties_dict)
+
+
+#add edge annotation properties that exist on both nodes of an edge. And remove it from the Networkx node property dictionary.
+to_remove = {}
+for edge in list(G.edges):
+    node_a = edge[0]
+    node_b = edge[1]
+    for prop in G.nodes[node_a]["properties"].keys():
+        intersection = G.nodes[node_a]["properties"][prop] & G.nodes[node_b]["properties"][prop]
+        #add intersection to edge property dictionary
+        if intersection:
+            G.add_edge(node_a,node_b,properties={prop:intersection})
+            if (node_a,prop) in to_remove.keys():
+                to_remove[(node_a,prop)] = to_remove[(node_a,prop)]|intersection
+            else:
+                to_remove[(node_a,prop)] = intersection
+            if (node_b,prop) in to_remove.keys():
+                to_remove[(node_b,prop)] = to_remove[(node_b,prop)]|intersection
+            else:
+                to_remove[(node_b,prop)] = intersection
+
+#remove properties form Networkx nodes that occur on both nodes of an edge (because it marks that property is only for the edge)
+for item in to_remove.keys():
+    node = item[0]
+    prop = item[1]
+    to_delete = to_remove[item]
+    G.nodes[node]["properties"][prop] = set(G.nodes[node]["properties"][prop] - set(to_delete))
