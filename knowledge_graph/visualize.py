7fa42b41cec3f085d6fafe260db7931ec0fc50fa
Kameron Rodrigues <kameronr@email.arizona.edu>
2020-08-08 21:46:32 -0700

    added features to dashboard
    added:
    - scroll
    - white background
    
    removed:
    - legend
    - weird spaces as tabs that caused bugs

--- knowledge_graph/visualize.py
+++ knowledge_graph/visualize.py
@@ -23,27 +23,27 @@ import matplotlib.pyplot as plt
 from scipy.special import binom
 
 def Bernstein(n, k):
-    """
-        Bernstein polynomial.
-    """
-    coeff = binom(n, k)
+	"""
+		Bernstein polynomial.
+	"""
+	coeff = binom(n, k)
 
-    def _bpoly(x):
-        return coeff * x ** k * (1 - x) ** (n - k)
+	def _bpoly(x):
+		return coeff * x ** k * (1 - x) ** (n - k)
 
-    return _bpoly
+	return _bpoly
 
 
 def Bezier(points, num=200):
-    """
-        Build Bézier curve from points.
-    """
-    N = len(points)
-    t = np.linspace(0, 1, num=num)
-    curve = np.zeros((num, 2))
-    for ii in range(N):
-        curve += np.outer(Bernstein(N - 1, ii)(t), points[ii])
-    return curve
+	"""
+		Build Bézier curve from points.
+	"""
+	N = len(points)
+	t = np.linspace(0, 1, num=num)
+	curve = np.zeros((num, 2))
+	for ii in range(N):
+		curve += np.outer(Bernstein(N - 1, ii)(t), points[ii])
+	return curve
 
 #load in networkx graph to access graph information
 G = nx.read_gpickle("Climate_Mind_DiGraph.gpickle")
@@ -80,73 +80,73 @@ s.close()
 N_nodes = []
 N_edges = []
 for item in content:
-    if " -> " in item:
-        N_edges.append(item)
-    else:
-        N_nodes.append(item)
+	if " -> " in item:
+		N_edges.append(item)
+	else:
+		N_nodes.append(item)
 
 #populate node graph layout details from graphviz
 N_node_details = []
 for N_node in N_nodes:
-    name = N_node.split('\t')[1].strip("\"")
-    node_attrs = N.get_node(name).attr
-    height = node_attrs.get("height", 0)
-    width = node_attrs.get("width", 0)
-    position = node_attrs.get("pos", []).split(",")
-    node_properties = G.nodes.get(name).get("properties")
-    node_properties_hovertext = "<br>".join([f"<b>{key}</b>: {val}" for (key, val) in node_properties.items()])
-    n_details = {
-    	"name": name,
-    	"position": {"x":float(position[0]), "y":float(position[1])},
-    	"height": float(height),
-    	"width": float(width),
-    	"node_properties_hovertext": node_properties_hovertext,
-    }
-    N_node_details.append(n_details)
+	name = N_node.split('\t')[1].strip("\"")
+	node_attrs = N.get_node(name).attr
+	height = node_attrs.get("height", 0)
+	width = node_attrs.get("width", 0)
+	position = node_attrs.get("pos", []).split(",")
+	node_properties = G.nodes.get(name).get("properties")
+	node_properties_hovertext = "<br>".join([f"<b>{key}</b>: {val}" for (key, val) in node_properties.items()])
+	n_details = {
+		"name": name,
+		"position": {"x":float(position[0]), "y":float(position[1])},
+		"height": float(height),
+		"width": float(width),
+		"node_properties_hovertext": node_properties_hovertext,
+	}
+	N_node_details.append(n_details)
 
 
 #populate edge graph layout details from graphviz
 N_edge_details = []
 for edge in N_edges:
-    node1,node2 = edge.split('\t')[1].split(' -> ')
-    node1 = node1.strip("\"")
-    node2 = node2.strip("\"")
+	node1,node2 = edge.split('\t')[1].split(' -> ')
+	node1 = node1.strip("\"")
+	node2 = node2.strip("\"")
 
-    edge_attrs = N.get_edge(node1, node2).attr
-    positions = edge_attrs.get("pos")\
-        .replace('e,', '')\
-        .replace('\\', '')\
-        .replace('r', '')\
-        .replace('n', '')
-    positions = [[float(x),float(y)] for (x,y) in [cp.split(",") for cp in positions.split(" ")]]
-    edge_type = edge_attrs.get("type")
-    edge_details = {
-    	"node1": node1,
-    	"node2": node2,
-    	"positions": positions,
-    	"edge_type": edge_type,
-    }
-    N_edge_details.append(edge_details)
+	edge_attrs = N.get_edge(node1, node2).attr
+	positions = edge_attrs.get("pos")\
+		.replace('e,', '')\
+		.replace('\\', '')\
+		.replace('r', '')\
+		.replace('n', '')
+	positions = [[float(x),float(y)] for (x,y) in [cp.split(",") for cp in positions.split(" ")]]
+	edge_type = edge_attrs.get("type")
+	edge_details = {
+		"node1": node1,
+		"node2": node2,
+		"positions": positions,
+		"edge_type": edge_type,
+	}
+	N_edge_details.append(edge_details)
 
 
 #divide the x and y coordinates into separate lists
 node_x_list = []
 node_y_list = []
 for node in N_node_details:
-    node_x_list.append(node.get("position").get("x"))
-    node_y_list.append(node.get("position").get("y"))
+	node_x_list.append(node.get("position").get("x"))
+	node_y_list.append(node.get("position").get("y"))
 
 
 #divide graphviz edge curve coordinates into groups of coordinates to help draw edges as correct spline curves (cubic B splines)
 def divide_into_4s(input):
-    size = 4
-    step = 3
-    output = [input[i : i + size] for i in range(1, len(input)-2, step)]
-    return(output)
+	size = 4
+	step = 3
+	output = [input[i : i + size] for i in range(1, len(input)-2, step)]
+	return(output)
 
 #unit vector to help with edge geometry (specifically drawing arrows)
 def unit_vector(v):
-    return v / np.linalg.norm(v)
+	return v / np.linalg.norm(v)
 
 
 def get_filtered_data(edge_type=None):
@@ -183,106 +183,106 @@ def get_figure(edge_type=None):
 	#Add node traces as ovals to the figure object
 	#Note how 72 is the conversion of graphviz point scale to inches scale
 	for node in N_node_details:
-	    # Do not show the nodes not in the_nodes_to_display
-	    if node.get("name") not in the_nodes_to_display:
-	    	continue
+		# Do not show the nodes not in the_nodes_to_display
+		if node.get("name") not in the_nodes_to_display:
+			continue
 
-	    fig.add_shape(
-	                  type="circle",
-	                  x0=node.get("position").get("x")-0.5*node.get("width")*72,
-	                  y0=node.get("position").get("y")-0.5*node.get("height")*72,
-	                  x1=node.get("position").get("x")+0.5*node.get("width")*72,
-	                  y1=node.get("position").get("y")+0.5*node.get("height")*72
-	                  )
+		fig.add_shape(
+					  type="circle",
+					  x0=node.get("position").get("x")-0.5*node.get("width")*72,
+					  y0=node.get("position").get("y")-0.5*node.get("height")*72,
+					  x1=node.get("position").get("x")+0.5*node.get("width")*72,
+					  y1=node.get("position").get("y")+0.5*node.get("height")*72
+					  )
 
 
-	    #add scatter trace of text labels to the figure object
-	    fig.add_trace(go.Scatter(
-	                             x=[node.get("position").get("x")],
-	                             y=[node.get("position").get("y")],
-	                             # https://plotly.com/python/hover-text-and-formatting/#customizing-hover-text-with-a-hovertemplate    
-	                             hovertemplate=node.get("node_properties_hovertext"),
-	                             text=node.get("name"),
-	                             mode="text",
-	                             textfont=dict(
-	                                           color="black",
-	                                           size=8.5,
-	                                           family="sans-serif",
-	                                           )
-	                             ))
+		#add scatter trace of text labels to the figure object
+		fig.add_trace(go.Scatter(
+								 x=[node.get("position").get("x")],
+								 y=[node.get("position").get("y")],
+								 # https://plotly.com/python/hover-text-and-formatting/#customizing-hover-text-with-a-hovertemplate    
+								 hovertemplate=node.get("node_properties_hovertext"),
+								 text=node.get("name"),
+								 mode="text",
+								 textfont=dict(
+											   color="black",
+											   size=8.5,
+											   family="sans-serif",
+											   )
+								 ))
 
 
 	#adding edges (and arrows and tees to edges)
 	for edge in N_edge_details:
-	    edge_position = edge.get("positions")
+		edge_position = edge.get("positions")
 
-	    # # Do not show the edges not in edges_to_display
-	    o_edge = N.get_edge(edge["node1"], edge["node2"])
-	    if o_edge not in the_edges_to_display:
-	    	continue
+		# # Do not show the edges not in edges_to_display
+		o_edge = N.get_edge(edge["node1"], edge["node2"])
+		if o_edge not in the_edges_to_display:
+			continue
 
-	    start = edge_position[0]
-	    end = edge_position[1]
-	    backwards = edge_position[2:][::-1]
-	    edge_fix = [start]+backwards+[end] #graphviz has weird edge coordinate format that doesn't have coordinates in correct order
-	    #approximate the B spline curve
-	    #see the following websites to better understand:
-	    #http://graphviz.996277.n3.nabble.com/how-to-draw-b-spline-td1328.html
-	    #https://stackoverflow.com/questions/28279060/splines-with-python-using-control-knots-and-endpoints
-	    #https://stackoverflow.com/questions/53934876/how-to-draw-a-graphviz-spline-in-d3
-	    #https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-837-computer-graphics-fall-2012/lecture-notes/MIT6_837F12_Lec01.pdf
-	    #https://github.com/kawache/Python-B-spline-examples
-	    #https://stackoverflow.com/questions/12643079/b%C3%A9zier-curve-fitting-with-scipy
-	    #https://nurbs-python.readthedocs.io/en/latest/module_bspline.html
-	    blocks = divide_into_4s(edge_fix)
-	    path = [] #path to draw
-	    path.append(start)
-	    for chunk in blocks:
-	        curve = Bezier(chunk,200)
-	        path = path + curve.tolist()
+		start = edge_position[0]
+		end = edge_position[1]
+		backwards = edge_position[2:][::-1]
+		edge_fix = [start]+backwards+[end] #graphviz has weird edge coordinate format that doesn't have coordinates in correct order
+		#approximate the B spline curve
+		#see the following websites to better understand:
+		#http://graphviz.996277.n3.nabble.com/how-to-draw-b-spline-td1328.html
+		#https://stackoverflow.com/questions/28279060/splines-with-python-using-control-knots-and-endpoints
+		#https://stackoverflow.com/questions/53934876/how-to-draw-a-graphviz-spline-in-d3
+		#https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-837-computer-graphics-fall-2012/lecture-notes/MIT6_837F12_Lec01.pdf
+		#https://github.com/kawache/Python-B-spline-examples
+		#https://stackoverflow.com/questions/12643079/b%C3%A9zier-curve-fitting-with-scipy
+		#https://nurbs-python.readthedocs.io/en/latest/module_bspline.html
+		blocks = divide_into_4s(edge_fix)
+		path = [] #path to draw
+		path.append(start)
+		for chunk in blocks:
+			curve = Bezier(chunk,200)
+			path = path + curve.tolist()
 
-	    edge_color = "black"
-	    #add arrow adornment using linear algebra
-	    if edge.get("edge_type") == 'causes_or_promotes':
-	        #A,B = [path[-20],path[-1]]
-	        A,B = [path[20],path[0]]
-	        A = np.array(A)
-	        B = np.array(B)
-	        height = 5*math.sqrt(3)
-	        theta = 45
-	        width = height*math.tan(theta/2)
-	        U = (B - A)/np.linalg.norm(B-A)
-	        V = np.array((-1*U[1], U[0]))
-	        v1 = B - height*U + width*V
-	        v2 = B - height*U - width*V
-	        adornment_to_add = [v1.tolist()]+[B]+[v2.tolist()]
-	        xpoint = [ coordinate[0] for coordinate in adornment_to_add ]
-	        ypoint = [ coordinate[1] for coordinate in adornment_to_add ]
-	        fig.add_trace(go.Scatter(x=xpoint,y=ypoint,line_shape='linear',mode='lines'))
-	        edge_color = "blue"
+		edge_color = "black"
+		#add arrow adornment using linear algebra
+		if edge.get("edge_type") == 'causes_or_promotes':
+			#A,B = [path[-20],path[-1]]
+			A,B = [path[20],path[0]]
+			A = np.array(A)
+			B = np.array(B)
+			height = 5*math.sqrt(3)
+			theta = 45
+			width = height*math.tan(theta/2)
+			U = (B - A)/np.linalg.norm(B-A)
+			V = np.array((-1*U[1], U[0]))
+			v1 = B - height*U + width*V
+			v2 = B - height*U - width*V
+			adornment_to_add = [v1.tolist()]+[B]+[v2.tolist()]
+			xpoint = [ coordinate[0] for coordinate in adornment_to_add ]
+			ypoint = [ coordinate[1] for coordinate in adornment_to_add ]
+			fig.add_trace(go.Scatter(x=xpoint,y=ypoint,line_shape='linear',mode='lines'))
+			edge_color = "blue"
 
-	    #add tee adornment using linear algebra
-	    if edge.get("edge_type") == 'is_inhibited_or_prevented_or_blocked_or_slowed_by':
-	        #B,A = [path[0],path[1]]
-	        B,A = [path[-1],path[2]]
-	        A = np.array(A)
-	        B = np.array(B)
-	        height = 0
-	        width = 10
-	        U = (B - A)/np.linalg.norm(B-A)
-	        V = np.array((-1*U[1], U[0]))
-	        v1 = B - height*U + width*V
-	        v2 = B - height*U - width*V
-	        adornment_to_add = [v1.tolist()]+[B]+[v2.tolist()]
-	        xpoint = [ coordinate[0] for coordinate in adornment_to_add ]
-	        ypoint = [ coordinate[1] for coordinate in adornment_to_add ]
-	        fig.add_trace(go.Scatter(x=xpoint,y=ypoint, line_shape='linear',mode='lines'))
-	        edge_color = "red"
+		#add tee adornment using linear algebra
+		if edge.get("edge_type") == 'is_inhibited_or_prevented_or_blocked_or_slowed_by':
+			#B,A = [path[0],path[1]]
+			B,A = [path[-1],path[2]]
+			A = np.array(A)
+			B = np.array(B)
+			height = 0
+			width = 10
+			U = (B - A)/np.linalg.norm(B-A)
+			V = np.array((-1*U[1], U[0]))
+			v1 = B - height*U + width*V
+			v2 = B - height*U - width*V
+			adornment_to_add = [v1.tolist()]+[B]+[v2.tolist()]
+			xpoint = [ coordinate[0] for coordinate in adornment_to_add ]
+			ypoint = [ coordinate[1] for coordinate in adornment_to_add ]
+			fig.add_trace(go.Scatter(x=xpoint,y=ypoint, line_shape='linear',mode='lines'))
+			edge_color = "red"
 
-	    #add edge spline trace to the figure object
-	    xp = [ coordinate[0] for coordinate in path ]
-	    yp = [ coordinate[1] for coordinate in path ]
-	    fig.add_trace(go.Scatter(x=xp,y=yp,marker=dict(color=edge_color), line_shape='spline'))
+		#add edge spline trace to the figure object
+		xp = [ coordinate[0] for coordinate in path ]
+		yp = [ coordinate[1] for coordinate in path ]
+		fig.add_trace(go.Scatter(x=xp,y=yp,marker=dict(color=edge_color), line_shape='spline'))
 
 
 
@@ -291,16 +291,17 @@ def get_figure(edge_type=None):
 	#change the x and y axis ranges to be the values found in the 'header' of the graphviz graph layout string
 	fig.update_xaxes(range=[0, 8395.7])
 	fig.update_yaxes(range=[0, 1404])
+	fig.update_layout(showlegend=False,plot_bgcolor='rgba(0,0,0,0)')
 	print("get_figure! FIG IS RETURNING")
 	return fig
 #may need to add this back in later to help adjust the first look of the dashboard
 #fig.update_layout(
-                    #autosize=False,
-                    #width=8395.7,
-                    #height=1404,
-                    #yaxis = dict(
-                    #scaleanchor = "x",
-                    #            scaleratio = /1404,
+					#autosize=False,
+					#width=8395.7,
+					#height=1404,
+					#yaxis = dict(
+					#scaleanchor = "x",
+					#            scaleratio = /1404,
 #            ))
 
 # #add scroll zooming as a feature
@@ -315,20 +316,21 @@ app = dash.Dash()
 
 # NEED TO ADD HTML formating and maybe CSS
 app.layout = html.Div(children=[
-    html.H1(children='Climate Mind DiGraph'),
-    dcc.Graph(
-        id='graph',
-        figure=get_figure()
-    ),
-    dcc.RadioItems(
-         id='edge-type-filter',
-         options=[
-             {'label': u'causes_or_promotes', 'value': 'causes_or_promotes'},
-             {'label': u'is_inhibited_or_prevented_or_blocked_or_slowed_by', 'value': 'is_inhibited_or_prevented_or_blocked_or_slowed_by'},
-             {'label': u'All', 'value': 'all'},
-         ],
-         value='all'
-    ),                                
+	html.H1(children='Climate Mind DiGraph'),
+	dcc.Graph(
+		id='graph',
+		figure=get_figure(),
+		config= dict({'scrollZoom': True})
+	),
+	dcc.RadioItems(
+		 id='edge-type-filter',
+		 options=[
+			 {'label': u'causes_or_promotes', 'value': 'causes_or_promotes'},
+			 {'label': u'is_inhibited_or_prevented_or_blocked_or_slowed_by', 'value': 'is_inhibited_or_prevented_or_blocked_or_slowed_by'},
+			 {'label': u'All', 'value': 'all'},
+		 ],
+		 value='all'
+	),                                
 ])
 
 
@@ -336,23 +338,23 @@ app.layout = html.Div(children=[
 # NEED TO ADD in callback features to make the dashboard interactive!!!
 
 @app.callback(
-    dash.dependencies.Output('graph', 'figure'),
-    [dash.dependencies.Input('edge-type-filter', 'value')])
+	dash.dependencies.Output('graph', 'figure'),
+	[dash.dependencies.Input('edge-type-filter', 'value')])
 def display_click_data(value):
-    print("display_click_data!")
-    # import ipdb
-    # ipdb.set_trace()
-    if not value:
-        # Nothing has to happen.
-        # otherwise the callback is called in some load/init cases
-        raise dash.exceptions.PreventUpdate
-    if value == "all":
-        value=None
-    print(f"display_click_data! FILTER VALUE = {value}")
-    return get_figure(value)
+	print("display_click_data!")
+	# import ipdb
+	# ipdb.set_trace()
+	if not value:
+		# Nothing has to happen.
+		# otherwise the callback is called in some load/init cases
+		raise dash.exceptions.PreventUpdate
+	if value == "all":
+		value=None
+	print(f"display_click_data! FILTER VALUE = {value}")
+	return get_figure(value)
 
 
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+	app.run_server(debug=False)
