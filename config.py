0b99dc0a95618ba4b45f0a2d984a2eaec589a4be
Sean Payne <sean.payne.es@gmail.com>
2020-09-07 22:16:47 +0900

    added database for users

--- config.py
+++ config.py
@@ -1,17 +1,23 @@
 from flask import abort
+import os
 
 from knowledge_graph.Mind import Mind
-from knowledge_graph import app
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig(object):
     DEBUG = False
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     TESTING = True
     try:
-        app.config["MIND"] = Mind()
+        MIND = Mind()
     except (FileNotFoundError, IsADirectoryError, ValueError):
         abort(404)
 
