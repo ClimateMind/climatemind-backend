3555fcea8e41da8b943a8437bc29f9ee9ec63868
Sean Payne <sean.payne.es@gmail.com>
2020-09-08 00:40:32 +0900

    Added test user, fixed issue with db

--- climatemind.py
+++ climatemind.py
@@ -1,8 +1,8 @@
 from knowledge_graph import app, db
-from knowledge_graph.models import User
+from knowledge_graph.models import User, Scores
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return {'db': db, 'User': User, 'Scores': Scores}
 
 app.run(debug=True, host="0.0.0.0")
