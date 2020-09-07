6dbde0c2bb0df1a96d267a08e2654a3c44772b0d
Sean Payne <sean.payne.es@gmail.com>
2020-09-07 16:05:32 +0900

    added a testing method

--- test_posting.py
+++ test_posting.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = 'http://127.0.0.1:5000/get_user_scores'
+url = 'http://127.0.0.1:5000/users/scores'
 
 with open('sample_user_response.json') as json_file:
     obj = json.load(json_file)
@@ -10,3 +10,11 @@ headers = {'content-type' : 'application/json'}
 request = requests.post(url, json=obj, headers=headers)
 
 print(json.dumps(request.json(), indent=4))
+
+url = 'http://127.0.0.1:5000/get_actions'
+
+with open('sample_user_scores.json') as json_file:
+    obj = json.load(json_file)
+
+headers = {'content-type' : 'application/json'}
+request = requests.post(url, json=obj, headers=headers)
