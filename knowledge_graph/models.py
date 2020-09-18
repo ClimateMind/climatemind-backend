8a8d7e477195e605ea348d329f51ad18314f23ff
Tom Deadman <tjdeadma@gmail.com>
2020-09-18 15:19:55 +0100

    added new scores columns

--- knowledge_graph/models.py
+++ knowledge_graph/models.py
@@ -5,36 +5,54 @@ from knowledge_graph import login
 
 """ Contains all of the table structures for the database. When these are updated
     two commands need to be run in terminal/console.
-    
+
     1) flask db migrate -m "leave a comment about changes here"
     2) flask db upgrade
-    
+
     This ensures that the database models are updated and ready to use.
 """
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    zip = db.Column(db.Integer, index=False, unique=False)
     scores = db.relationship('Scores', backref='owner', lazy='dynamic')
-    
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-    
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
+
     def __repr__(self):
         """ Tells Python how to print """
         return '<User {}>'.format(self.username)
+
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
+
 class Scores(db.Model):
+    session_id = db.Column(db.Integer, primary_key=True)
+    security = db.Column(db.String(64), index=True, unique=True)
+    conformity = db.Column(db.String(64), index=True, unique=True)
+    benevolence = db.Column(db.String(64), index=True, unique=True)
+    tradition = db.Column(db.String(64), index=True, unique=True)
+    universalism = db.Column(db.String(64), index=True, unique=True)
+    self_direction = db.Column(db.String(64), index=True, unique=True)
+    stimulation = db.Column(db.String(64), index=True, unique=True)
+    hedonism = db.Column(db.String(64), index=True, unique=True)
+    achievement = db.Column(db.String(64), index=True, unique=True)
+    power = db.Column(db.String(64), index=True, unique=True)
+
+
+class LRF(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    score = db.Column(db.Float, index=False, unique=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    iri = db.Column(db.String(120), index=False, unique=False)
+    zip = db.Column(db.Integer, index=True, unique=False)
+    affected_by_iri = db.Column(db.Boolean, index=False, unique=False)
