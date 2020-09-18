f2560f1aabb68fb68a0645795fd83d1eaf2669a3
Tom Deadman <tjdeadma@gmail.com>
2020-09-18 17:09:49 +0100

    iterating on persist_scores test

--- knowledge_graph/models.py
+++ knowledge_graph/models.py
@@ -21,7 +21,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     zip = db.Column(db.Integer, index=False, unique=False)
-    scores = db.relationship('Scores', backref='owner', lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -40,7 +39,7 @@ def load_user(id):
 
 
 class Scores(db.Model):
-    session_id = db.Column(db.Integer, ForeignKey(User.scores), primary_key=True)
+    session_id = db.Column(db.Integer, primary_key=True)
     security = db.Column(db.String(64), index=True, unique=True)
     conformity = db.Column(db.String(64), index=True, unique=True)
     benevolence = db.Column(db.String(64), index=True, unique=True)
--- knowledge_graph/persist_scores.py
+++ knowledge_graph/persist_scores.py
@@ -1,16 +1,14 @@
 from typing import Type
 
 from knowledge_graph import db
-from knowledge_graph.models import User, Scores
-
+from knowledge_graph.models import Scores
 
 def persist_scores(scores: dict) -> Type[KeyError]:
     try:
+        s = Scores()
+        s.id = scores['session-id']
 
-        u = User()
-        u.id = scores['session-id']
-
-        db.session.add(u)
+        db.session.add(s)
         db.session.commit()
 
         for key, value in scores:
