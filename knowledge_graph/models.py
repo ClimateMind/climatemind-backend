67f63128f8d58599f3191a7bf57d94e71302cbbf
Tom Deadman <tjdeadma@gmail.com>
2020-09-18 13:23:42 +0100

    scores model changed to include personality columns

--- knowledge_graph/models.py
+++ knowledge_graph/models.py
@@ -38,10 +38,17 @@ def load_user(id):
 
 
 class Scores(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    score = db.Column(db.Float, index=False, unique=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    session_id = db.Column(db.Integer, primary_key=True)
+    security = db.Column(db.String(64), index=True, unique=True)
+    conformity = db.Column(db.String(64), index=True, unique=True)
+    benevolence = db.Column(db.String(64), index=True, unique=True)
+    tradition = db.Column(db.String(64), index=True, unique=True)
+    universalism = db.Column(db.String(64), index=True, unique=True)
+    self_direction = db.Column(db.String(64), index=True, unique=True)
+    stimulation = db.Column(db.String(64), index=True, unique=True)
+    hedonism = db.Column(db.String(64), index=True, unique=True)
+    achievement = db.Column(db.String(64), index=True, unique=True)
+    power = db.Column(db.String(64), index=True, unique=True)
 
 
 class LRF(db.Model):
