ea6b250eae35596643565f9dea34a3da2e32f7e3
Sean Payne <sean.payne.es@gmail.com>
2020-09-24 10:48:24 +0900

    removing blocking file

--- db_test_scripts/create_temp_scores.py
+++
@@ -1,7 +0,0 @@
-from knowledge_graph import db
-from knowledge_graph.models import Scores
-
-def main():
-s = Scores(session_id = 1, security = 4.6, conformity = 3.2, benevolence = 1, tradition = 3.3, universalism = 1.2, self_direction = 1.5, stimulation = .3, hedonism = .4, achievement = 2.0, power = .3)
-db.session.add(s)
-db.session.commit()
