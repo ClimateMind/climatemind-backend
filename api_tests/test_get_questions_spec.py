a55339b2b857fdd7ce20315ce57361b090e61b87
Sean Payne <sean.payne.es@gmail.com>
2020-10-20 22:53:36 +0900

    linter

--- api_tests/test_get_questions_spec.py
+++ api_tests/test_get_questions_spec.py
@@ -1,18 +1,18 @@
 # import requests
 # import json
 # import time
-# 
-# 
+#
+#
 # def test_get_questions():
 #     url = "http://127.0.0.1:5000/questions"
 #     resp = requests.get(url)
 #     # Check we get the correct response
 #     assert resp.status_code == 200
-# 
+#
 #     # Check we have the correct number of questions in each set
 #     assert len(resp.json()["SetOne"]) == 10
 #     assert len(resp.json()["SetTwo"]) == 10
-# 
+#
 #     # Check answers are returned correctly
 #     assert resp.json()["Answers"] == [
 #         {"id": 1, "text": "Not Like Me At All"},
@@ -22,16 +22,16 @@
 #         {"id": 5, "text": "Like Me"},
 #         {"id": 6, "text": "Very Much Like Me"},
 #     ]
-# 
+#
 #     # Check that the two sets have the same question values
 #     for question in resp.json()["SetOne"]:
 #         assert question["value"] == resp.json()["SetTwo"][question["id"] - 1]["value"]
-# 
-# 
+#
+#
 # def test_questions_endpoint_parameters():
 #     url = "http://127.0.0.1:5000/questions"
 #     parameters = ["admin", "edit", "1"]
-# 
+#
 #     for parameter in parameters:
 #         resp = requests.get(url + parameter)
 #         assert resp.status_code == 500
