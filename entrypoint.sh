ca4c4fa0b6317e01592be9405003ea0ec47be6fa
Kameron Rodrigues <kameronr@email.arizona.edu>
2020-10-30 19:14:31 -0700

    creates database named sqldb-web-prod-001

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 #flask db init
+
+/opt/mssql-tools/bin/sqlcmd -S tcp:db -U SA -P Cl1mat3m1nd! -Q "CREATE DATABASE [sqldb-web-prod-001]"
+
+
 flask db migrate -m "added all tables"
 flask db upgrade
 flask run --host=0.0.0.0
+
+
---
+++ testConnection.py
@@ -0,0 +1,24 @@
+import pyodbc
+
+# Some other example server values are
+# server = 'localhost\sqlexpress' # for a named instance
+# server = 'myserver,port' # to specify an alternate port
+server = 'tcp:db' 
+database = 'sqldb-web-prod-001' 
+username = 'sa' 
+password = 'Cl1mat3m1nd!' 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password, autocommit=True)
+cursor = cnxn.cursor()
+
+#Sample select query
+result1 = cursor.execute("SELECT @@version;") 
+result2 = cursor.execute("SELECT name FROM SYSOBJECTS WHERE xtype='U';") 
+#result3 = cursor.execute('CREATE DATABASE "sqldb-web-prod-001";') 
+print(list(result2))
+
+# row = cursor.fetchone() 
+# while row: 
+#     print(row[0])
+#     row = cursor.fetchone()
+
+
