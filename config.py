2ad012d8db7479ab357f6ebd93da86523333c484
Kameron Rodrigues <kameronr@email.arizona.edu>
2020-10-30 22:59:06 -0700

    Database fixes
    * corrected main database connection configuration
    * fixed openSSL issue (I think was an issue?)
    * removed getSession (no more declarative sqlalquemy!)
    * cleaned up database transaction sessions
    * cleaned up database queries
    * updated migrations

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from flask import abort
 import os
+import urllib
 
 from knowledge_graph.Mind import Mind
 
@@ -16,10 +17,17 @@ class DevelopmentConfig(BaseConfig):
     # basedir = os.path.abspath(os.path.dirname(__file__))
     # SQLALCHEMY_DATABASE_URI = #"sqlite:///" + os.path.join(basedir, "app.db")
     # SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DATABASE_PARAMS = os.environ["DATABASE_PARAMS"]
-    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % DATABASE_PARAMS
+    #DATABASE_PARAMS = os.environ["DATABASE_PARAMS"]
+    #SQLALCHEMY_DATABASE_URI = "mssql+pyodbc:///?odbc_connect=%s" % DATABASE_PARAMS
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    DB_CREDENTIALS = os.environ.get("DATABASE_PARAMS")
+    SQLALCHEMY_DATABASE_URI = (
+        "mssql+pyodbc:///?odbc_connect=%s" % urllib.parse.quote_plus(DB_CREDENTIALS)
+    )
+    #SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("%", "%%")
+
     # End Temporary
 
     try:
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 sleep 15
 /opt/mssql-tools/bin/sqlcmd -S tcp:db -U SA -P Cl1mat3m1nd! -Q "CREATE DATABASE [sqldb-web-prod-001]"
 
-
 flask db migrate -m "added all tables"
 flask db upgrade
 flask run --host=0.0.0.0
