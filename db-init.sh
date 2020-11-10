9f16f9354e083f6f5545cf384bfe43e43fa04a2e
Andrew Banister <awban22@gmail.com>
2020-11-10 00:40:36 -0800

    install wait-for-it
    ordered better the database timing for loading dependancies and intializations.
    
    Now there is just errors occuring from migrations and the python app (no errors from the database initialization)

--- db-init.sh
+++ db-init.sh
@@ -1,6 +1,5 @@
 #wait for the SQL Server to come up
-echo "sleeping DB Init script"
+echo "sleeping for mssql server to initialize system databases"
 sleep 30
-
 echo "running DB Init script"
 /opt/mssql-tools/bin/sqlcmd -S tcp:db -U SA -P Cl1mat3m1nd! -Q "CREATE DATABASE [sqldb-web-prod-001]"
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+#wait for database to start
+#sleep 15
+
 flask db upgrade
 flask run --host=0.0.0.0
