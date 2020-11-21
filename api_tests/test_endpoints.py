ab8afa11e4d86fa1765888cc0a0c25adafd05fd7
seanmajorpayne <seanmajorpayne@gmail.com>
2020-11-21 19:57:45 +0900

    test issue

--- api_tests/test_endpoints.py
+++ api_tests/test_endpoints.py
@@ -1,30 +1,31 @@
 import requests
 import json
 
-url = "http://127.0.0.1:5000/scores"
+def run_this():
+    url = "http://127.0.0.1:5000/scores"
 
-with open("../json_files/sample_user_response.json") as json_file:
-    obj = json.load(json_file)
+    with open("../json_files/sample_user_response.json") as json_file:
+        obj = json.load(json_file)
 
-headers = {"content-type": "application/json"}
-request = requests.post(url, json=obj, headers=headers)
+    headers = {"content-type": "application/json"}
+    request = requests.post(url, json=obj, headers=headers)
 
-response = json.loads(json.dumps(request.json()))
-print("Session ID Returned as ", response)
+    response = json.loads(json.dumps(request.json()))
+    print("Session ID Returned as ", response)
 
-session_id = response["sessionId"]
+    session_id = response["sessionId"]
 
-url = "http://127.0.0.1:5000/personal_values"
-params = {"session-id": session_id}
-request = requests.get(url, params=params)
+    url = "http://127.0.0.1:5000/personal_values"
+    params = {"session-id": session_id}
+    request = requests.get(url, params=params)
 
-print(json.dumps(request.json()))
+    print(json.dumps(request.json()))
 
-url = "http://127.0.0.1:5000/feed"
-params = {"session-id": session_id}
-request = requests.get(url, params=params)
+    url = "http://127.0.0.1:5000/feed"
+    params = {"session-id": session_id}
+    request = requests.get(url, params=params)
 
-print(json.dumps(request.json(), indent=4))
+    print(json.dumps(request.json(), indent=4))
 
 
 def test_endpoints():
