0db49181f09aea4e21be5be4e2c34c943ac6a4b3
Kameron Rodrigues <kameronr@email.arizona.edu>
2020-12-12 01:50:06 -0800

    adaptation solutions added to effect nodes in networkx

--- knowledge_graph/make_graph.py
+++ knowledge_graph/make_graph.py
@@ -338,6 +338,7 @@ def makeGraph(onto_path, edge_path, output_folder_path):
     to_remove = set_edge_properties(G)
     remove_edge_properties_from_nodes(G, to_remove)
 
+    # process the mitigation and adaptation solutions in the networkx object and add them into special attribute fields for each node for easy access in later for the API
     B = G.copy()
     # identify nodes that are in the class 'feedback loop' then remove those nodes' 'caueses' edges because they start feedback loops.
     nx.get_node_attributes(B, "direct classes")
@@ -397,6 +398,66 @@ def makeGraph(onto_path, edge_path, output_folder_path):
             ):  # bad to hard code in 'is_inhibited_or_prevented_or_blocked_or_slowed_by'
                 mitigation_solutions.append(neighbor)
 
+    # update the networkx object to have a 'mitigation solutions' field and include in it all nodes from mitigation_solutions
+    nx.set_node_attributes(
+        G,
+        {"increase in greenhouse effect": mitigation_solutions},
+        "mitigation solutions",
+    )
+    # to check or obtain the solutions from the networkx object: G.nodes['increase in greenhouse effect']['mitigation solutions']
+    breakpoint()
+
+    # code to get the adaptation solutions from a node in the networkx object:
+
+    # use the pruned copy of the graph (use B) because it has thee feedback loop edges removed.
+    # Get the upstream edges of the node in the B graph that occur between the node in question and the node 'increase in greenhouse effect'
+    # print(list(nx.dfs_edges(B,”starting node name”))) #this is to get downstream edges
+    # to do this could remove the parent edges of the 'increase in greenhouse effect' node?
+    # adaptation is defined as solutions to the node or any node upstream of the node up until the node 'increase in greenhouse effect'.
+
+    # get all the nodes that are downstream of 'increase in greenhouse effect'. should be all the impact/effect node... could probably get these by doing class search too
+    downstream_nodes = nx.dfs_edges(B, "increase in greenhouse effect")
+    downstream_nodes = [item for sublist in downstream_nodes for item in sublist]
+    nodes_downstream_greenhouse_effect = list(OrderedDict.fromkeys(downstream_nodes))
+    for effectNode in nodes_downstream_greenhouse_effect:
+        # breakpoint()
+        intermediate_nodes = nx.all_simple_paths(
+            B, "increase in greenhouse effect", effectNode
+        )
+        # collapse nested lists and remove duplicates
+        intermediate_nodes = [
+            item for sublist in intermediate_nodes for item in sublist
+        ]
+        intermediate_nodes = list(
+            dict.fromkeys(intermediate_nodes)
+        )  # gets unique nodes
+        node_adaptation_solutions = list()
+        for intermediateNode in intermediate_nodes:
+            # if intermediateNode == 'increase in area burned by wildfire': breakpoint()
+            node_neighbors = G.neighbors(intermediateNode)
+            for neighbor in node_neighbors:
+                if (
+                    G[intermediateNode][neighbor]["type"]
+                    == "is_inhibited_or_prevented_or_blocked_or_slowed_by"
+                ):  # bad to hard code in 'is_inhibited_or_prevented_or_blocked_or_slowed_by'
+                    node_adaptation_solutions.append(neighbor)
+        # add the adaptation solutions to the networkx object for the node
+        # breakpoint()
+        # be sure that solutions don't show up as effectNodes! and that they aren't solutions to themself! the code needs to be changed to avoid this.
+        # ^solutions shouldn't be added as solutions to themself!
+        node_adaptation_solutions = list(
+            dict.fromkeys(node_adaptation_solutions)
+        )  # gets unique nodes
+        # print(str(effectNode)+": "+str(node_adaptation_solutions))
+
+        # need to add a check here that doesn't add to effectNode attributes the effectNode as an adaptation solution (solution nodes should have themself as an adaptation solution!)
+        nx.set_node_attributes(
+            G, {effectNode: node_adaptation_solutions}, "adaptation solutions"
+        )
+    # to check or obtain the solutions from the networkx object: G.nodes[node]['adaptation solutions']
+    # ex: G.nodes['decrease in test scores']['adaptation solutions']
+    # should probably code in for the 'adaptation solutions' field to read "None yet curated" if there isn't any to avoid errors from occuring later by API ?
+    # G.nodes['increase in area burned by wildfire']['adaptation solutions'] should not return *** KeyError: 'adaptation solutions'
     breakpoint()
 
     # B.nodes['permafrost melt']['direct classes']
