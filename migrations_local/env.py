75568263a6627be4d86d2aa70a80c5e202f9770f
yhimanen <y.himanen@gmail.com>
2021-01-01 18:07:44 +0200

    created script to add lrf_data table from csv to local db whenever docker is spun up

--- migrations_local/env.py
+++ migrations_local/env.py
@@ -10,6 +10,7 @@ from alembic import context
 import os
 import urllib
 
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -107,3 +108,4 @@ if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
+
