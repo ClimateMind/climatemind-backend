fff7776cdd454aae7ae0fd1ab6bea1283fbddf97
Kameron Rodrigues <kameronr@email.arizona.edu>
2021-01-18 15:59:35 -0800

    barebones setup.py for pip install to work

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='climatemind-backend',
+    version='1.0.0',
+    description='Climate Mind webapp code',
+    author='ClimateMind',
+    url='https://github.com/ClimateMind/climatemind-backend',
+    packages=find_packages(exclude=["docs","tests", ".gitignore", "README.rst","DESCRIPTION.rst"])
+)
