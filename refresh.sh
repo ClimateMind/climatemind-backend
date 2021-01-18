25f6062cece73d065e375485fc055ac8ec526e44
Ben Davis <bengineerdavis@gmail.com>
2021-01-17 19:23:45 -0500

    Adds temp function for refreshing debug session

--- refresh.sh
+++ refresh.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#continu!/bin/bash
 
 # I need to refresh every time I change code or docker won't import it into the web service
 
@@ -20,5 +20,5 @@ DO="docker exec -it $WEB python -m"
 # temp dependencies I didn't want to add into requirements.txt
 # and, run debugger on script that calls make_graph.py
 $DO pip install ipdb ipython && \
-$DO ipdb process_new_ontology_file.py
+$DO ipdb -c continue process_new_ontology_file.py
 
