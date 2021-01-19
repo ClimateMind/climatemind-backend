3c2f7f4ca148af84e5eb29776455c8894d581fda
Sean Payne <sean.payne.es@gmail.com>
2021-01-20 00:21:55 +0900

    linting

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
-setup(name='climatemind-backend',
-    version='1.0.0',
-    description='Climate Mind webapp code',
-    author='ClimateMind',
-    url='https://github.com/ClimateMind/climatemind-backend',
-    py_modules=["process_new_ontology_and_visualize","process_new_ontology_file","config"],
-    packages=find_packages(exclude=["docs","tests", ".gitignore", "README.rst","DESCRIPTION.rst"])
+setup(
+    name="climatemind-backend",
+    version="1.0.0",
+    description="Climate Mind webapp code",
+    author="ClimateMind",
+    url="https://github.com/ClimateMind/climatemind-backend",
+    py_modules=[
+        "process_new_ontology_and_visualize",
+        "process_new_ontology_file",
+        "config",
+    ],
+    packages=find_packages(
+        exclude=["docs", "tests", ".gitignore", "README.rst", "DESCRIPTION.rst"]
+    ),
 )
