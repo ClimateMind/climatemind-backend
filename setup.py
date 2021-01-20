1cdc7fecdac5abf324694ccc0712fcb5a77df270
Sean Payne <sean.payne.es@gmail.com>
2021-01-20 12:34:54 +0900

    moved ont processing

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ setup(
     author="ClimateMind",
     url="https://github.com/ClimateMind/climatemind-backend",
     py_modules=[
-        "process_new_ontology_and_visualize",
-        "process_new_ontology_file",
         "config",
     ],
     packages=find_packages(
