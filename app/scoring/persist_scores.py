2f4513f1a19685a90243e509ab47123130b91206
yhimanen <y.himanen@gmail.com>
2021-03-01 00:55:51 +0200

    CM-522 db refactoring, all string conversions of uuid changed to uuid, timestamps added

--- app/scoring/persist_scores.py
+++ app/scoring/persist_scores.py
@@ -1,3 +1,5 @@
+import datetime
+from datetime import timezone
 from typing import Type
 
 from app import db
@@ -9,13 +11,13 @@ def persist_scores(scores: dict) -> Type[KeyError]:
         # db.session = getSession()
 
         userSession = Sessions()
-        userSession.session_id = scores["session-id"]
+        userSession.session_uuid = scores["session-id"]
 
         db.session.add(userSession)
         # db.session.commit()
 
         userScores = Scores()
-        userScores.session_id = scores["session-id"]
+        userScores.session_uuid = scores["session-id"]
         userScores.security = scores["security"]
         userScores.conformity = scores["conformity"]
         userScores.benevolence = scores["benevolence"]
@@ -26,6 +28,7 @@ def persist_scores(scores: dict) -> Type[KeyError]:
         userScores.hedonism = scores["hedonism"]
         userScores.achievement = scores["achievement"]
         userScores.power = scores["power"]
+        userScores.scores_created_timestamp = datetime.datetime.now(timezone.utc)
 
         db.session.add(userScores)
         db.session.commit()
--- app/subscribe/routes.py
+++ app/subscribe/routes.py
@@ -6,6 +6,8 @@ from app.subscribe.store_subscription_data import store_subscription_data
 
 from app import auto
 
+import uuid
+
 
 @bp.route("/subscribe", methods=["POST"])
 @auto.doc()
@@ -13,8 +15,9 @@ def subscribe():
     try:
         request_body = request.json
         email = request_body["email"]
-        session_id = request_body["sessionId"]
-        response = store_subscription_data(session_id, email)
+        session_uuid = uuid.UUID(request_body["sessionId"])
+        breakpoint()
+        response = store_subscription_data(session_uuid, email)
         return response
     except:
         return {"error": "Invalid request"}, 400
