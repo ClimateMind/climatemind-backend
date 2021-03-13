f5c3ecea985d662de66dd205129c0f098fb29a9b
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-03-13 17:19:04 +0000

    add cy test for both scores

--- cypress/integration/scores.spec.js
+++ cypress/integration/scores.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import scores from "../fixtures/postScores.json";
+import scoresSetTwo from "../fixtures/postScoresSetTwo.json";
 
 let sessionId; // store the session id from the post to chech we can retreive the values and scores.
 
@@ -8,6 +9,19 @@ describe("Scores endpoint", () => {
   it("can Post Scores", () => {
     // Write Tests
     cy.request("POST", "/scores", scores).should((response) => {
+      expect(response.status).to.equal(201);
+      expect(response.headers["content-type"]).to.equal("application/json");
+      expect(response.headers["access-control-allow-origin"]).to.equal("*");
+      expect(response.body).to.be.a("object");
+      expect(response.body).to.have.property("sessionId");
+      expect(response.body.sessionId).to.be.a("string");
+      sessionId = response.body["sessionId"];
+    });
+  });
+
+  it("can post both sets of scores", () => {
+    // Write Tests
+    cy.request("POST", "/scores", scoresSetTwo).should((response) => {
       expect(response.status).to.equal(201);
       expect(response.headers["content-type"]).to.equal("application/json");
       expect(response.headers["access-control-allow-origin"]).to.equal("*");
