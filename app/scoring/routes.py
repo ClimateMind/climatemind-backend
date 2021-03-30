7d2bdeb8ea0130d67a9418c78da6224e08769aa6
seanmajorpayne <seanmajorpayne@gmail.com>
2021-03-30 22:40:20 +0900

    scoring refactor

--- app/scoring/routes.py
+++ app/scoring/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request
+from flask import jsonify, request, abort
 
 import uuid
 import os
@@ -11,102 +11,160 @@ from app.post_code.store_post_code import store_post_code
 
 from app import auto
 
+value_id_map = {
+    1: "conformity",
+    2: "tradition",
+    3: "benevolence",
+    4: "universalism",
+    5: "self-direction",
+    6: "stimulation",
+    7: "hedonism",
+    8: "achievement",
+    9: "power",
+    10: "security",
+}
+
 
 @bp.route("/scores", methods=["POST"])
 @auto.doc()
 def user_scores():
-    """Users want to be able to get their score results after submitting
-    the survey. This method checks for a POST request from the front-end
+    """
+    User scores are used to determine which solutions are best to serve
+    the user. Users also want to be able to see their score results after
+    submitting the survey.
+
+    This route checks for a POST request from the front-end
     containing a JSON object with the users scores.
+
     The user can answer 10 or 20 questions. If they answer 20, the scores
     are averaged between the 10 additional and 10 original questions to get
     10 corresponding value scores.
+
     Then to get a centered score for each value, each score value is subtracted
-    from the overall average of all 10 or 20 questions. This is returned to the
-    front-end.
+    from the overall average of all 10 or 20 questions.
+
+    A session ID is saved with the scores in the database.
+
+    Returns: SessionID (UUID4)
     """
-    try:
-        parameter = request.json
-    # todo: handle exceptions properly here
-    except:
-        return {"error": "Invalid user response"}, 400
 
-    value_scores = {}
-    overall_sum = 0
-    num_of_responses = 10
+    parameter = request.json
+    if not parameter:
+        abort(400, description="No user response provided")
 
-    NUMBER_OF_SETS = 2
-    POSITIVITY_CONSTANT = 3.5
-    RESPONSES_TO_ADD = 10
-
-    session_uuid = uuid.uuid4()
+    responses_to_add = 10
 
     questions = parameter["questionResponses"]
-    postal_code = parameter["zipCode"]
 
-    if len(questions["SetOne"]) < RESPONSES_TO_ADD:
-        return {"error": "Not enough set one scores"}, 400
-    elif len(questions["SetOne"]) > RESPONSES_TO_ADD:
-        return {"error": "Too many set one scores"}, 400
+    if len(questions["SetOne"]) != responses_to_add:
+        abort(400, description="Invalid number of questions provided")
 
-    value_id_map = {
-        1: "conformity",
-        2: "tradition",
-        3: "benevolence",
-        4: "universalism",
-        5: "self-direction",
-        6: "stimulation",
-        7: "hedonism",
-        8: "achievement",
-        9: "power",
-        10: "security",
-    }
+    value_scores, overall_avg = calculate_scores(questions)
+    value_scores = center_scores(value_scores, overall_avg)
 
-    for value in questions["SetOne"]:
-        questionID = value["questionId"]
-        score = value["answerId"]
-        overall_sum += score
-
-        if value_id_map[questionID] in value_scores:
-            return {"error": "Duplicate question ID"}, 400
-
-        value_scores[value_id_map[questionID]] = score
-
-    if "SetTwo" in questions:
-        num_of_responses += RESPONSES_TO_ADD
-        for value in questions["SetTwo"]:
-            questionID = value["questionId"]
-            score = value["answerId"]
-            name = value_id_map[questionID]
-            avg_score = (value_scores[name] + score) / NUMBER_OF_SETS
-            overall_sum += score
-
-            if questionID in value_scores.keys():
-                return {"error": "Duplicate question ID"}, 400
-
-            value_scores[name] = avg_score
-
-    overall_avg = overall_sum / num_of_responses
-
-    for value, score in value_scores.items():
-        centered_score = (
-            score - overall_avg + POSITIVITY_CONSTANT
-        )  # To make non-negative
-
-        value_scores[value] = centered_score
-
+    session_uuid = uuid.uuid4()
     value_scores["session-id"] = session_uuid
 
     try:
         persist_scores(value_scores)
-    except KeyError:
-        return {"error": "Invalid key"}, 400
+    except:
+        abort(500, description="Unable to save provided question data")
+
+    postal_code = parameter["zipCode"]
 
     if postal_code:
         try:
             store_post_code(postal_code, session_uuid)
         except:
-            return {"error": "Error adding zipcode to db"}, 500
+            abort(500, description="Error adding zipcode to db")
+
+    process_ip_address(request, session_uuid)
+
+    response = {"sessionId": session_uuid}
+    return jsonify(response), 201
+
+
+def calculate_scores(
+    questions, num_of_sets=2, num_of_responses=10, responses_to_add=10
+):
+    """
+    Creates a dictionary of personal values and scores based on the user's responses.
+
+    Args:
+        questions: JSON data with questions, IDs and self-evaluated scores
+        num_of_sets: Number of 10 question sets (Int)
+        num_of_responses: How many questions the user answered (Int)
+        responses_to_add: How many responses should be calculated (Int)
+
+    Returns: value_scores - A dictionary of personal values and scores
+             overall_avg - overall_sum / num_of_responses
+
+    """
+    overall_sum = 0
+    value_scores = {}
+
+    for value in questions["SetOne"]:
+        question_id = value["questionId"]
+        name = value_id_map[question_id]
+        score = value["answerId"]
+        overall_sum += score
+
+        if value_id_map[question_id] in value_scores:
+            abort(400, description="Duplicate question ID")
+        value_scores[name] = score
+
+    if "SetTwo" in questions:
+        if len(questions["SetTwo"]) != responses_to_add:
+            abort(400, description="Invalid number of questions provided")
+
+        num_of_responses += responses_to_add
+        for value in questions["SetTwo"]:
+            question_id = value["questionId"]
+            score = value["answerId"]
+            name = value_id_map[question_id]
+            avg_score = (value_scores[name] + score) / num_of_sets
+            overall_sum += score
+
+            if question_id in value_scores.keys():
+                abort(400, description="Duplicate question ID")
+
+            value_scores[name] = avg_score
+
+    overall_avg = overall_sum / num_of_responses
+
+    return value_scores, overall_avg
+
+
+def center_scores(value_scores, overall_avg, positivity_constant=3.5):
+    """
+    User scores need to be non-negative and balanced based on their overall average score.
+
+    Args:
+        value_scores: A dictionary of personal values (str) and their scores (int)
+        overall_avg: overall_sum (Int) / num_of_responses (Int)
+        positivity_constant: Int
+
+    Returns: value_scores
+
+    """
+    for value, score in value_scores.items():
+        centered_score = score - overall_avg + positivity_constant
+
+        value_scores[value] = centered_score
+        return value_scores
+
+
+def process_ip_address(request, session_uuid):
+    """
+    Save a user's IP address information into the database with their session_id.
+    Provided credentials are for locally generated database (not production).
+
+    Args:
+        request: Request
+        session_uuid: UUID4
+
+    Returns: Error and Status Code if they exist, otherwise None
+    """
 
     if (
         os.environ["DATABASE_PARAMS"]
@@ -116,7 +174,7 @@ def user_scores():
             ip_address = None
             store_ip_address(ip_address, session_uuid)
         except:
-            return {"error": "Error adding ip address locally"}, 500
+            abort(500, description="Error adding ip address locally")
     else:
         try:
             unprocessed_ip_address = request.headers.getlist("X-Forwarded-For")
@@ -127,9 +185,4 @@ def user_scores():
                 ip_address = None
             store_ip_address(ip_address, session_uuid)
         except:
-            return {"error": "Error adding ip address in cloud"}, 500
-
-    response = {"sessionId": session_uuid}
-
-    response = jsonify(response)
-    return response, 201
+            abort(500, description="Error adding ip address in cloud")
