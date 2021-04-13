dc25aa375ee850c117535a49400f06c3dbb3ec2f
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-04-13 15:04:27 +0100

    lint

--- app/personal_values/routes.py
+++ app/personal_values/routes.py
@@ -12,12 +12,11 @@ from app import auto
 
 
 def normalize_scores(values):
-    min_score = min([value['score'] for value in values])
-    max_score = max([value['score'] for value in values])
+    min_score = min([value["score"] for value in values])
+    max_score = max([value["score"] for value in values])
     # TODO: Check this correct way to normalise scores
     for val in values:
-        val['score'] = (val['score'] - min_score) / \
-            (max_score - min_score) * 10
+        val["score"] = (val["score"] - min_score) / (max_score - min_score) * 10
     return values
 
 
@@ -54,14 +53,17 @@ def get_personal_values():
         scores = scores.__dict__
 
         # All scores and accoiated values for response
-        all_scores = [{'personalValue': key, 'score': scores[key]}
-                      for key in personal_values_categories]
+        all_scores = [
+            {"personalValue": key, "score": scores[key]}
+            for key in personal_values_categories
+        ]
 
         normalized_scores = normalize_scores(all_scores)
 
         # Top 3 personal values
-        top_scores = sorted(
-            all_scores, key=lambda value: value['score'], reverse=True)[:3]
+        top_scores = sorted(all_scores, key=lambda value: value["score"], reverse=True)[
+            :3
+        ]
 
         # Fetch descriptions
         try:
@@ -75,13 +77,14 @@ def get_personal_values():
         print(top_scores)
 
         # Add desciptions for top 3 values to retrun
-        values_and_descriptions = [value_descriptions[score['personalValue']]
-                                   for score in top_scores]
+        values_and_descriptions = [
+            value_descriptions[score["personalValue"]] for score in top_scores
+        ]
 
         # Build and return response
         response = {
             "personalValues": values_and_descriptions,
-            "valueScores": normalized_scores
+            "valueScores": normalized_scores,
         }
         return jsonify(response), 200
 
