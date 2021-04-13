14c89436dea80ca825ce879dccda4996330e42f9
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-04-13 18:36:58 +0100

    fix normalise function

--- app/personal_values/normalize.py
+++ app/personal_values/normalize.py
@@ -1,8 +1,15 @@
 def normalize_scores(values):
+    # This normalise function rescales the users score between the users score
+    range_min = 0
+    range_max = 6
     min_score = min([value["score"] for value in values])
     max_score = max([value["score"] for value in values])
-    # TODO: Check this correct way to normalise scores
+
     for val in values:
-        normalized_value = (val["score"] - min_score) / (max_score - min_score) * 10
+        normalized_value = range_min + (val["score"] - min_score) * \
+            (range_max - range_min) / (max_score - min_score)
+
         val["score"] = round(normalized_value, 1)
+        print(val['score'])
+
     return values
