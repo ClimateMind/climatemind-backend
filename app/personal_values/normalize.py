314941f5cfd663d1a999dbb136339e704f208423
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-04-13 17:01:05 +0100

    lint

--- app/personal_values/normalize.py
+++ app/personal_values/normalize.py
@@ -3,6 +3,5 @@ def normalize_scores(values):
     max_score = max([value["score"] for value in values])
     # TODO: Check this correct way to normalise scores
     for val in values:
-        val["score"] = (val["score"] - min_score) / \
-            (max_score - min_score) * 10
+        val["score"] = (val["score"] - min_score) / (max_score - min_score) * 10
     return values
