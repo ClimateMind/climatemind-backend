63c8e58633e51180a8b1fa3f53b0e573ff8a6ac3
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-04-13 16:53:23 +0100

    Move norm func

---
+++ app/personal_values/normalize.py
@@ -0,0 +1,8 @@
+def normalize_scores(values):
+    min_score = min([value["score"] for value in values])
+    max_score = max([value["score"] for value in values])
+    # TODO: Check this correct way to normalise scores
+    for val in values:
+        val["score"] = (val["score"] - min_score) / \
+            (max_score - min_score) * 10
+    return values
--- app/personal_values/routes.py
+++ app/personal_values/routes.py
@@ -10,14 +10,7 @@ from app.models import Scores
 
 from app import auto
 
-
-def normalize_scores(values):
-    min_score = min([value["score"] for value in values])
-    max_score = max([value["score"] for value in values])
-    # TODO: Check this correct way to normalise scores
-    for val in values:
-        val["score"] = (val["score"] - min_score) / (max_score - min_score) * 10
-    return values
+from app.personal_values.normalize import normalize_scores
 
 
 @bp.route("/personal_values", methods=["GET"])
