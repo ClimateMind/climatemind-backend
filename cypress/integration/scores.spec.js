d2e56e715cde10247458a83462e007944c1c523c
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-04-29 23:38:28 +0100

    Remove port

--- cypress/integration/scores.spec.js
+++ cypress/integration/scores.spec.js
@@ -34,46 +34,44 @@ describe("Scores endpoint", () => {
   });
 
   it("retreive the personal values for those scores", () => {
-    cy.request(
-      `http://localhost:5000/personal_values?session-id=${sessionId}`
-    ).should((response) => {
-      expect(response.status).to.equal(200);
-      expect(response.headers["content-type"]).to.equal("application/json");
-      expect(response.headers["access-control-allow-origin"]).to.equal("*");
-      expect(response.body).to.be.a("object");
-      expect(response.body).to.have.property("personalValues");
-      expect(response.body.valueScores).to.have.length(10);
-      const { personalValues } = response.body;
-      const { valueScores } = response.body;
-      // P Values have all the right properties
-      personalValues.forEach((value) => {
-        expect(value).to.have.property("id");
-        expect(value).to.have.property("description");
-        expect(value).to.have.property("shortDescription");
-        expect(value).to.have.property("name");
-      });
-      // Scores have all the right properties and valid values
-      valueScores.forEach((score) => {
-        expect(score).to.have.property("personalValue");
-        expect(score).to.satisfy(function (s) {
-          const isValid =
-            typeof s.score === "number" && s.score >= 0 && s.score <= 10;
-          return isValid;
-        });
-      });
-    });
-  });
-
-  it("retreive the climate feed for those scores", () => {
-    cy.request(`http://localhost:5000/feed?session-id=${sessionId}`).should(
+    cy.request(`/personal_values?session-id=${sessionId}`).should(
       (response) => {
         expect(response.status).to.equal(200);
         expect(response.headers["content-type"]).to.equal("application/json");
         expect(response.headers["access-control-allow-origin"]).to.equal("*");
         expect(response.body).to.be.a("object");
-        expect(response.body).to.have.property("climateEffects");
+        expect(response.body).to.have.property("personalValues");
+        expect(response.body.valueScores).to.have.length(10);
+        const { personalValues } = response.body;
+        const { valueScores } = response.body;
+        // P Values have all the right properties
+        personalValues.forEach((value) => {
+          expect(value).to.have.property("id");
+          expect(value).to.have.property("description");
+          expect(value).to.have.property("shortDescription");
+          expect(value).to.have.property("name");
+        });
+        // Scores have all the right properties and valid values
+        valueScores.forEach((score) => {
+          expect(score).to.have.property("personalValue");
+          expect(score).to.satisfy(function (s) {
+            const isValid =
+              typeof s.score === "number" && s.score >= 0 && s.score <= 10;
+            return isValid;
+          });
+        });
       }
     );
+  });
+
+  it("retreive the climate feed for those scores", () => {
+    cy.request(`/feed?session-id=${sessionId}`).should((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.headers["content-type"]).to.equal("application/json");
+      expect(response.headers["access-control-allow-origin"]).to.equal("*");
+      expect(response.body).to.be.a("object");
+      expect(response.body).to.have.property("climateEffects");
+    });
   });
 
   it("It accepts a null zip code", () => {
@@ -94,17 +92,17 @@ describe("Scores endpoint", () => {
       sessionId = response.body["sessionId"];
 
       // Check the personal values scores for the corresponding sessions are correct
-      cy.request(
-        `http://localhost:5000/personal_values?session-id=${sessionId}`
-      ).should((response) => {
-        expect(response.status).to.equal(200);
-        // Each personal value has a score in the middle of the range.
-        const { valueScores } = response.body;
-        valueScores.forEach((score) => {
-          expect(score).to.have.property("personalValue");
-          expect(score.score).to.equal(3);
-        });
-      });
+      cy.request(`/personal_values?session-id=${sessionId}`).should(
+        (response) => {
+          expect(response.status).to.equal(200);
+          // Each personal value has a score in the middle of the range.
+          const { valueScores } = response.body;
+          valueScores.forEach((score) => {
+            expect(score).to.have.property("personalValue");
+            expect(score.score).to.equal(3);
+          });
+        }
+      );
     });
   });
 });
