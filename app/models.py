da33f63819e3478441a5d7c8d1aa0381a11f5010
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-05-03 18:59:38 +0100

    comment out unused imports

--- app/models.py
+++ app/models.py
@@ -2,12 +2,12 @@ import os
 from flask import current_app
 from app.extensions import db, login, jwt
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+# from flask_login import UserMixin
 
 # Azure
 # from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER
 
 
