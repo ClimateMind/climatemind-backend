31b63f4825981a84f58ffef24f0f88d5f4aef86a
seanmajorpayne <seanmajorpayne@gmail.com>
2021-05-04 08:14:37 +0900

    fix cypress

--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -1,13 +1,13 @@
 /// <reference types="cypress" />
 
 const user1 = {
-  full-name: "user one"
+  full-name: "user one",
   email: "test1@example.com",
   password: "PassWord7!",
 };
 
 const user2 = {
-  full-name: "user two"
+  full-name: "user two",
   email: "test2@example.com",
   password: "PassWord7!",
 };
