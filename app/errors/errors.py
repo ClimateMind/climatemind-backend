2c9ec4b622001b6ca713030b3af738f94b09ec0e
seanmajorpayne <seanmajorpayne@gmail.com>
2021-05-04 14:56:40 +0900

    fixing errors

--- app/errors/errors.py
+++ app/errors/errors.py
@@ -1,21 +1,22 @@
 class CustomError(Exception):
+    status_code = 500
+
     def __init__(self, message):
         Exception.__init__(self)
-        self.status_code = 500
         self.message = message
 
 
 class UnauthorizedError(CustomError):
-    self.status_code = 401
+    status_code = 401
 
 
 class InvalidUsageError(CustomError):
-    self.status_code = 400
+    status_code = 400
 
 
 class DatabaseError(CustomError):
-    self.status_code = 500
+    status_code = 500
 
 
 class AlreadyExistsError(CustomError):
-    self.status_code = 409
+    status_code = 409
