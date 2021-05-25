15c182e684402d27b7ae74bc718bd540b485b94b
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-05-25 22:17:44 +0100

    remove comments

--- cypress/integration/logout.spec.js
+++ cypress/integration/logout.spec.js
@@ -1,10 +1,5 @@
 /// <reference types="cypress" />
 
-// Expected error responses
-const badLoginMessage = "Wrong email or password. Try again.";
-const invalidReqMessage =
-  "Email and password must included in the request body";
-
 describe("/logout", () => {
   it("User can log out", () => {
     cy.request("POST", "http://localhost:5000/logout").should((response) => {
