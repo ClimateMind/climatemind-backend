095f02ed35ce7f2b1d0e34fd5fff944f01486e4a
Henry Nguyen <henry2833@gmail.com>
2021-06-14 22:12:00 -0700

    more email error handling

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,5 +1,8 @@
+import app
 from app import db
-from flask import jsonify, request
+from flask import jsonify, request, current_app
+import flask
+import json
 from app.errors import bp
 from app.errors.errors import DatabaseError, AlreadyExistsError, CustomError
 from flask_cors import cross_origin
@@ -9,26 +12,36 @@ from logging.handlers import SMTPHandler
 
 
 class EmailErrorFormatter(logging.Formatter):
+    # Formats error logs to include request information (e.g. URL, HTTP method)
+    # Converts the whole `request` dict into string format
     def format(self, record):
-        record.error_string = str(request.__dict__)
+        if flask.has_request_context():
+            record.error_string = json.dumps(
+                request.__dict__, indent=4, default=lambda o: f"{str(type(o))}"
+            )
+        else:
+            record.error_string = "Not in request"
         return super().format(record)
 
 
 def register_mail_handler(app):
+    # Registers the `app.logger` instance to use another handler at the "Error" log level.
+    # Will email these log messages via default `SMTPHandler` to admin.
     mail_handler = SMTPHandler(
-        mailhost="127.0.0.1",
-        fromaddr="server-error@example.com",
-        toaddrs=["admin@example.com"],
+        # Replace mailhost with (addr, port) of correct SMTP server.
+        mailhost=("127.0.0.1", 1025),
+        fromaddr="climatemind-server@climatemind.org",
+        toaddrs=["hello@climatemind.org"],
         subject="Application Error",
     )
+
     mail_handler.setLevel(logging.ERROR)
     mail_handler.setFormatter(
-        logging.Formatter(
+        EmailErrorFormatter(
             "[%(asctime)s] %(levelname)s in %(module)s: %(message)s\n"
-            "Request information: %(error_string)"
+            "Request information: %(error_string)s"
         )
     )
-
     app.logger.addHandler(mail_handler)
 
 
@@ -36,6 +49,8 @@ def register_mail_handler(app):
 @cross_origin()
 def handle_custom_error(error):
     response = jsonify({"error": error.message}), error.status_code
+    current_app.logger.error(response)
+
     return response
 
 
@@ -44,6 +59,8 @@ def handle_custom_error(error):
 def handle_database_error(error):
     db.session.rollback()
     response = jsonify({"error": error.message}), error.status_code
+
+    current_app.logger.error(response)
     return response
 
 
@@ -54,4 +71,6 @@ def handle_existing_resource_error(error):
         jsonify({"error": error.message + " already exists in the database."}),
         error.status_code,
     )
+
+    current_app.logger.error(response)
     return response
