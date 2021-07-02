635d968735e82f90bca86193035680b739c9fdd8
Nick Callaghan <nicholas.callaghan@gmail.com>
2021-07-02 15:13:55 +0100

    update origins

--- cypress/integration/logout.spec.js
+++ cypress/integration/logout.spec.js
@@ -6,7 +6,7 @@ describe("/logout", () => {
       expect(response.status).to.equal(200);
       expect(response.headers["content-type"]).to.equal("application/json");
       expect(response.headers["access-control-allow-origin"]).to.equal(
-        "http://localhost:3000"
+        "http://0.0.0.0:3000"
       );
       expect(response.body).to.be.a("object");
       expect(response.body).to.have.property("message");
