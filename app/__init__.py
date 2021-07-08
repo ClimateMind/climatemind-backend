893fabb7ac6a455cc71c82e17f1233091900af6a
seanmajorpayne <seanmajorpayne@gmail.com>
2021-07-08 20:18:12 +0900

    lint

--- app/__init__.py
+++ app/__init__.py
@@ -51,17 +51,21 @@ def create_app(config_class=DevelopmentConfig):
 
     # Error Logger Outputs to Logs Directory
 
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/cm_error.log', maxBytes=10240,
-                                       backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+    file_handler = RotatingFileHandler(
+        "logs/cm_error.log", maxBytes=10240, backupCount=10
+    )
+    file_handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+        )
+    )
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
-    app.logger.info('ClimateMind Startup')
+    app.logger.info("ClimateMind Startup")
 
     with app.app_context():
 
