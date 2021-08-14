97fb9766cfd164d66558896bdb3b1d05108eab12
seanmajorpayne <seanmajorpayne@gmail.com>
2021-08-14 19:06:25 +0900

    adding UUID checks for each endpoint

--- app/subscribe/routes.py
+++ app/subscribe/routes.py
@@ -4,6 +4,7 @@ from app.subscribe import bp
 
 from app.subscribe.store_subscription_data import store_subscription_data, check_email
 from app.errors.errors import InvalidUsageError
+from app.auth.utils import uuidType, validate_uuid
 from flask_cors import cross_origin
 
 from app import auto
@@ -15,14 +16,14 @@ import uuid
 @cross_origin()
 @auto.doc()
 def subscribe():
-    try:
-        request_body = request.json
-        email = request_body["email"]
-        session_uuid = uuid.UUID(request_body["sessionId"])
-    except:
-        raise InvalidUsageError(
-            message="Unable to post subscriber information. Check the request parameters."
-        )
+    r = request.get_json(force=True, silent=True)
+
+    if not r:
+        raise InvalidUsageError(message="JSON body must be included.")
+
+    email = r.get("email", None)
+    session_uuid = r.get("sessionId", None)
+    validate_uuid(session_uuid, uuidType.SESSION)
 
     if check_email(email):
         return store_subscription_data(session_uuid, email)
