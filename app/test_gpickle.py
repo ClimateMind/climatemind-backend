0b64cd0dcacc2c2e9dda975af583b7d7ecccb175
Moataz Mahmoud <moatazmasters@gmail.com>
2021-10-10 15:23:24 +0200

    fixing linting issues

--- app/test_gpickle.py
+++ app/test_gpickle.py
@@ -8,8 +8,8 @@ from app.network_x_tools.network_x_processor import network_x_processor
 
 
 def test_me():
-  nx_processor = network_x_processor()
-  G = nx_processor.get_graph()
-  print('from inside the test')
-  # breakpoint()
-  assert True
+    nx_processor = network_x_processor()
+    G = nx_processor.get_graph()
+    print("from inside the test")
+    # breakpoint()
+    assert True
