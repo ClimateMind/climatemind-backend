d52462485870fbae7f806eb89e07d2ff89b4f495
seanmajorpayne <seanmajorpayne@gmail.com>
2021-10-11 14:13:44 +0900

    testing gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ if [ "$FLASK_ENV" = "production"]
 then 
 	gunicorn --bind 0.0.0.0:5000 climatemind:app
 else
-	flask run --host=0.0.0.0
+	gunicorn --bind 0.0.0.0:5000 climatemind:app
 fi
 
 # NOTE: migrate and upgrade must be run SEPARATELY for cloud migrations.
