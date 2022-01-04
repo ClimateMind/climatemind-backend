e60ea91a9fdc4d4ecccef2e87798959734e79fd8
Yasmine Himanen <y.himanen@gmail.com>
2022-01-04 22:53:57 +0200

    post alignment endpoint updated, data saved to db, analytics tracking, dummy functions for calculating alignment scores and feed

---
+++ app/scoring/process_alignment_scores.py
@@ -0,0 +1,42 @@
+from app import db
+from app.errors.errors import DatabaseError
+from app.models import AlignmentScores
+
+
+def create_alignment_scores(conversation_uuid, quiz_uuid, alignment_scores_uuid):
+    """
+    Calculate aligned scores based on user a and b quiz results and add to the alignment scores table.
+
+    This is currently a dummy function.
+
+    Parameters
+    ==============
+    conversation_uuid (UUID)
+    quiz_uuid (UUID) - user b quiz uuid to compare scores with user a scores
+    alignment_scores_uuid (UUID) - uuid created when post alignment endpoint is used
+    """
+    # TODO: Add logic to create aligned scores. Currently working with hard-coded dummy values.
+
+    try:
+        alignment_scores = AlignmentScores()
+        alignment_scores.alignment_scores_uuid = alignment_scores_uuid
+        alignment_scores.overall_similarity_score = 80
+        alignment_scores.top_match_percent = 90
+        alignment_scores.top_match_value = "benevolence"
+        alignment_scores.benevolence_alignment = 90
+        alignment_scores.security_alignment = 85
+        alignment_scores.self_direction_alignment = 80
+        alignment_scores.power_alignment = 75
+        alignment_scores.universalism_alignment = 70
+        alignment_scores.achievement_alignment = 65
+        alignment_scores.conformity_alignment = 60
+        alignment_scores.tradition_alignment = 55
+        alignment_scores.hedonism_alignment = 50
+        alignment_scores.stimulation_alignment = 45
+
+        db.session.add(alignment_scores)
+        db.session.commit()
+    except:
+        raise DatabaseError(
+            message="An error occurred while adding scores to the alignment scores table."
+        )
