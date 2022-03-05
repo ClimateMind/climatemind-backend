7729159d9cd8e4f9cdb968f2288f28c3b63ae9e8
Kameron Rodrigues <kameronr@email.arizona.edu>
2022-03-04 17:46:55 -0800

    Lint and doc string documentation

--- app/scoring/tests/data.py
+++ app/scoring/tests/data.py
@@ -1,44 +1,47 @@
 import random
 
+
 def _generate_set(questions_range: tuple):
-        """
-        Creates random answers for a single question set of 10 questions.
+    """
+    Creates random answers for a single question set of 10 questions.
 
-        Args:
-            questions_range: a tuple for the range of question ids to use for creating the answers object
+    Args:
+        questions_range: a tuple for the range of question ids to use for creating the answers object
 
-        Returns: a single question set of random answers between 1 to 6
+    Returns: a single question set of random answers between 1 to 6
 
-        """    
+    """
     answer_object = [
-            {"questionId": qid, "answerId": aid}
-            for qid, aid in zip(
-                range(questions_range[0], questions_range[1] + 1),
-                [random.randint(1, 6) for _ in range(10)],
-            )
-        ]
+        {"questionId": qid, "answerId": aid}
+        for qid, aid in zip(
+            range(questions_range[0], questions_range[1] + 1),
+            [random.randint(1, 6) for _ in range(10)],
+        )
+    ]
 
     return answer_object
 
+
 def generate_random_answers():
-        """
-        Creates scores object mocking user who randomly answers either only the first question set of 10 questions or both set one and set two of 10 questions. Formats the result properly for answer scoring and processing with the app.
-        """ 
+    """
+    Creates scores object mocking user who randomly answers either only the first question set of 10 questions or both set one and set two of 10 questions. Formats the result properly for answer scoring and processing with the app.
+    """
     res = {"SetOne": _generate_set((1, 10))}
     with_set_two = bool(random.getrandbits(1))
     if with_set_two:
         res["SetTwo"] = _generate_set((11, 20))
     return res
 
+
 def generate_test_answers_10(a, b, c, d, e, f, g, h, i, j):
-        """
-        Creates scores object mocking user who only answers the first question set of 10 questions .
+    """
+    Creates scores object mocking user who only answers the first question set of 10 questions .
 
-        Args:
-            a-j: integer - Valid options are 1-6 where 1 is 'Not Like Me At All' and 6 is 'Very Much Like Me'
+    Args:
+        a-j: integer - Valid options are 1-6 where 1 is 'Not Like Me At All' and 6 is 'Very Much Like Me'
 
-        Returns: scores object properly formatted for scoring function for the app
-        """
+    Returns: scores object properly formatted for scoring function for the app
+    """
     answer_object = []
     answer_object.append({"questionId": 1, "answerId": a})
     answer_object.append({"questionId": 2, "answerId": b})
@@ -51,27 +54,20 @@ def generate_test_answers_10(a, b, c, d, e, f, g, h, i, j):
     answer_object.append({"questionId": 9, "answerId": i})
     answer_object.append({"questionId": 10, "answerId": j})
 
-    {"questionId": qid, "answerId": aid}
-
-    {"questionId": qid, "answerId": aid}
-            for qid, aid in zip(
-                range(questions_range[0], questions_range[1] + 1),
-                [random.randint(1, 6) for _ in range(10)],
-            )
-
-    [random.randint(1, 6) for _ in range(10)]
+    return answer_object
 
 
+def generate_test_answers_20(
+    a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t
+):
+    """
+    Creates scores object mocking user who answers set one and set two question sets of 10 questions.
 
-def generate_test_answers_20(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t):
-        """
-        Creates scores object mocking user who answers set one and set two question sets of 10 questions.
+    Args:
+        a-t: integer - Valid options are 1-6 where 1 is 'Not Like Me At All' and 6 is 'Very Much Like Me'
 
-        Args:
-            a-t: integer - Valid options are 1-6 where 1 is 'Not Like Me At All' and 6 is 'Very Much Like Me'
-
-        Returns: scores object properly formatted for scoring function for the app
-        """
+    Returns: scores object properly formatted for scoring function for the app
+    """
     answer_object_1 = generate_test_answers_10(a, b, c, d, e, f, g, h, i, j)
     answer_object_2 = []
     answer_object.append({"questionId": 11, "answerId": k})
@@ -90,26 +86,40 @@ def generate_test_answers_20(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, 
     return final_answer_object
 
 
-
 if __name__ == "__main__":
     process_scores_data = []
     number_of_random_quizzes = 5
-    
-    process_scores_data.append(generate_test_answers_10(6,6,6,6,6,6,6,6,1,1))
-    process_scores_data.append(generate_test_answers_20(6,6,6,6,6,6,6,6,1,1,6,6,6,6,6,6,6,6,1,1))
-    process_scores_data.append(generate_test_answers_10(6,6,6,6,6,6,6,6,6,1))
-    process_scores_data.append(generate_test_answers_20(6,6,6,6,6,6,6,6,6,1,6,6,6,6,6,6,6,6,6,1))
-    process_scores_data.append(generate_test_answers_10(6,6,6,6,6,1,1,1,1,1))
-    process_scores_data.append(generate_test_answers_20(6,6,6,6,6,1,1,1,1,1,6,6,6,6,6,1,1,1,1,1))
-    process_scores_data.append(generate_test_answers_10(1,1,1,1,1,1,1,1,1,1))
-    process_scores_data.append(generate_test_answers_20(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1))
-    process_scores_data.append(generate_test_answers_10(4,4,4,4,4,4,4,4,4,4))
-    process_scores_data.append(generate_test_answers_20(4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4))
+
+    process_scores_data.append(generate_test_answers_10(6, 6, 6, 6, 6, 6, 6, 6, 1, 1))
+    process_scores_data.append(
+        generate_test_answers_20(
+            6, 6, 6, 6, 6, 6, 6, 6, 1, 1, 6, 6, 6, 6, 6, 6, 6, 6, 1, 1
+        )
+    )
+    process_scores_data.append(generate_test_answers_10(6, 6, 6, 6, 6, 6, 6, 6, 6, 1))
+    process_scores_data.append(
+        generate_test_answers_20(
+            6, 6, 6, 6, 6, 6, 6, 6, 6, 1, 6, 6, 6, 6, 6, 6, 6, 6, 6, 1
+        )
+    )
+    process_scores_data.append(generate_test_answers_10(6, 6, 6, 6, 6, 1, 1, 1, 1, 1))
+    process_scores_data.append(
+        generate_test_answers_20(
+            6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1
+        )
+    )
+    process_scores_data.append(generate_test_answers_10(1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+    process_scores_data.append(
+        generate_test_answers_20(
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+        )
+    )
+    process_scores_data.append(generate_test_answers_10(4, 4, 4, 4, 4, 4, 4, 4, 4, 4))
+    process_scores_data.append(
+        generate_test_answers_20(
+            4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
+        )
+    )
 
     for _ in range(1, number_of_random_quizzes + 1):
         process_scores_data.append(generate_random_answers())
-
-   
-
-
-
