1787410a435c27e49f72eff5b8674dda73577807
Yasmine Himanen <y.himanen@gmail.com>
2022-04-02 16:47:32 +0300

    use kendal tau-b for similarity score calculation

--- app/scoring/process_alignment_scores.py
+++ app/scoring/process_alignment_scores.py
@@ -1,6 +1,7 @@
 from app import db
 from app.errors.errors import DatabaseError
-from app.models import AlignmentScores
+from app.models import AlignmentScores, Conversations, Scores, Users
+from scipy.stats import kendalltau
 
 
 def create_alignment_scores(conversation_uuid, quiz_uuid, alignment_scores_uuid):
@@ -20,7 +21,9 @@ def create_alignment_scores(conversation_uuid, quiz_uuid, alignment_scores_uuid)
     try:
         alignment_scores = AlignmentScores()
         alignment_scores.alignment_scores_uuid = alignment_scores_uuid
-        alignment_scores.overall_similarity_score = 0.7
+        alignment_scores.overall_similarity_score = calculate_overall_similarity_score(
+            conversation_uuid, quiz_uuid
+        )
         alignment_scores.top_match_percent = 90
         alignment_scores.top_match_value = "benevolence"
         alignment_scores.benevolence_alignment = 0.9
@@ -40,3 +43,70 @@ def create_alignment_scores(conversation_uuid, quiz_uuid, alignment_scores_uuid)
         raise DatabaseError(
             message="An error occurred while adding scores to the alignment scores table."
         )
+
+
+def calculate_overall_similarity_score(conversation_uuid, user_b_quiz_uuid):
+    """
+    Calculate the overall similarity score based on user b and user a's quiz results.
+
+    Parameters
+    ==========
+    conversation_uuid (UUID)
+    user_b_quiz_uuid (UUID)
+
+    Returns
+    ==========
+    overall_similarity_score (float) - calculated using the Kendall Tau-B model, transformed (+1) and scaled (/2)
+    """
+
+    conversation, user_a_quiz_uuid = (
+        db.session.query(Conversations, Users.quiz_uuid)
+        .join(Users, Users.user_uuid == Conversations.sender_user_uuid)
+        .filter(Conversations.conversation_uuid == conversation_uuid)
+        .one_or_none()
+    )
+
+    user_a_scores_list = get_scores_list(user_a_quiz_uuid)
+    user_b_scores_list = get_scores_list(user_b_quiz_uuid)
+
+    overall_similarity_score = (
+        kendalltau(user_a_scores_list, user_b_scores_list).correlation + 1
+    ) / 2
+
+    return overall_similarity_score
+
+
+def get_scores_list(quiz_uuid):
+    """
+    Get a list of a user's quiz scores, ordered alphabetically by personal values.
+
+    Parameters
+    ==========
+    quiz_uuid (UUID)
+
+    Returns
+    ==========
+    scores_list - a list of floats
+    """
+    personal_values_categories = [
+        "achievement",
+        "benevolence",
+        "conformity",
+        "hedonism",
+        "power",
+        "security",
+        "self_direction",
+        "stimulation",
+        "tradition",
+        "universalism",
+    ]
+
+    user_scores = (
+        db.session.query(Scores).filter(Scores.quiz_uuid == quiz_uuid).one_or_none()
+    )
+
+    user_scores = user_scores.__dict__
+
+    scores_list = [user_scores[value] for value in personal_values_categories]
+
+    return scores_list
