ee074d9cba84bd6ef99b7587c491840bf1914590
Yasmine Himanen <y.himanen@gmail.com>
2022-04-26 20:54:44 +0300

    commit to pull latest changes

--- app/alignment/utils.py
+++ app/alignment/utils.py
@@ -1,4 +1,5 @@
 import os
+from app.scoring.build_localised_acyclic_graph import get_node_id
 import numpy as np
 from json import load
 from urllib import response
@@ -12,6 +13,7 @@ from app.personal_values.utils import get_value_descriptions_map
 from app.models import (
     AlignmentScores,
     EffectChoice,
+    Scores,
     SolutionChoice,
     UserBJourney,
     Conversations,
@@ -605,26 +607,80 @@ def get_aligned_scores(alignment_scores):
     return aligned_scores
 
 
-def vectorise(scores_list):
-    """Create a vector from a numpy array."""
-
-    return np.array(scores_list).reshape(-1, 1)
-
-
-def transform_aligned_scores(scores_vector):
+def transform_aligned_scores(scores_array):
     """Transform the aligned scores for user a and b for all personal values by normalising the data to be within a range of 1-6 and squaring the scores to magnify their mathematical power."""
 
+    scores_array = scores_array.reshape(-1, 1)
     scaler = preprocessing.MinMaxScaler(feature_range=(1, 6))
-    scaled_aligned_scores = scaler.fit_transform(scores_vector)
-    transformed_aligned_scores = np.square(scaled_aligned_scores)
+    scaled_aligned_scores = scaler.fit_transform(scores_array)
+    squared_aligned_scores = np.square(scaled_aligned_scores)
+    transformed_aligned_scores = np.squeeze(np.asarray(squared_aligned_scores))
 
     return transformed_aligned_scores
 
 
-def calculate_dot_product(vector1, vector2):
-    """Transform vectors into numpy arrays and calculate their dot product."""
+def sort_aligned_effects_by_user_b_values(aligned_effects, conversation_uuid):
+    """Reorder the aligned effects for users a and b according to user b's personal value scores.
 
-    array1 = np.squeeze(np.asarray(vector1))
-    array2 = np.squeeze(np.asarray(vector2))
+    Parameters
+    ==========
+    aligned_effects - a list of IRIs for aligned effects ordered using the dot product of the users' aligned scores and the effects personal value associations
+    conversation_uuid (UUID) - the uuid for the conversation between user a and b
 
-    return np.dot(array1, array2)
+    Returns
+    ==========
+    sorted_aligned_effects - a dictionary of IRIs and dot products for the aligned effects and user b's personal value scores
+
+    """
+
+    G = current_app.config["G"].copy()
+
+    user_b_journey, user_b_scores = (
+        db.session.query(UserBJourney, Scores)
+        .join(Scores, Scores.quiz_uuid == UserBJourney.quiz_uuid)
+        .filter(UserBJourney.conversation_uuid == conversation_uuid)
+        .one_or_none()
+    )
+
+    sorted_aligned_effects = dict()
+
+    user_b_scores = np.array(
+        [
+            user_b_scores.achievement,
+            user_b_scores.benevolence,
+            user_b_scores.conformity,
+            user_b_scores.hedonism,
+            user_b_scores.power,
+            user_b_scores.security,
+            user_b_scores.self_direction,
+            user_b_scores.stimulation,
+            user_b_scores.tradition,
+            user_b_scores.universalism,
+        ]
+    )
+
+    modified_user_b_scores = np.square(user_b_scores)
+
+    for aligned_effect in aligned_effects:
+        for node in G.nodes:
+            current_node = G.nodes[node]
+
+            if get_node_id(current_node) == aligned_effect:
+                node_values_associations_10 = np.array(
+                    current_node["personal_values_10"]
+                )
+                node_values_associations_10 = np.where(
+                    node_values_associations_10 < 0,
+                    2 * node_values_associations_10,
+                    node_values_associations_10,
+                )
+                dot_product = np.dot(
+                    node_values_associations_10, modified_user_b_scores
+                )
+                sorted_aligned_effects[get_node_id(current_node)] = dot_product
+
+    sorted_aligned_effects = dict(
+        sorted(sorted_aligned_effects.items(), key=lambda x: x[1], reverse=True)
+    )
+
+    return sorted_aligned_effects
