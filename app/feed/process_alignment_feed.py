9ad541c7fb3f306c9e974abac8f9819eaee10386
Kameron Rodrigues <kameronr@email.arizona.edu>
2022-04-27 19:09:58 -0700

    Fixed bug and logic
    Bug = code was looking for value in the database that had not yet been
    written to the db.
    
    Logic missing = The top 3 shared topics are supposed to be ordered via
    version 1 scoring using user b’s scores. Previously, the code was
    ordering all topics with the shared personal value using version 1
    scoring, which close but not what we were aiming for as outlined in
    CM-921
    
    Additional logic needed = The sorting function results changed to
    returned lists instead of dictionaries because only iri are needed and
    dictionaries caused errors. This is minor, but necessary because if
    there are less than 3 topics possible, 1 topic needs to be repeated and
    if dictionaries are used then the topic can’t be repeated because
    dictionaries can’t have duplicate keys. When the ontology is expanded
    this can be reverted back to dictionaries instead of lists returned.

--- app/feed/process_alignment_feed.py
+++ app/feed/process_alignment_feed.py
@@ -36,16 +36,12 @@ def create_alignment_feed(
     quiz_uuid (UUID) - user b quiz uuid to compare scores with user a scores
     alignment_feed_uuid (UUID) - uuid created when post alignment endpoint is used
     """
+    
 
-    aligned_effects = list(get_aligned_effects(alignment_scores_uuid).keys())
+    #TO DO make a contant variable so 3 isn't hard coded as the number of effects to show to user B.
+    aligned_effects_sorted_by_shared_values = get_aligned_effects(alignment_scores_uuid, 3)
 
-    sorted_aligned_effects = list(
-        sort_aligned_effects_by_user_b_values(aligned_effects, conversation_uuid).keys()
-    )
-
-    # TODO: delete this check after expansion of the ontology
-    while len(sorted_aligned_effects) < 3:
-        aligned_effects.append(sorted_aligned_effects[0])
+    sorted_aligned_effects = sort_aligned_effects_by_user_b_values(aligned_effects_sorted_by_shared_values, quiz_uuid)
 
     try:
         alignment_feed = AlignmentFeed()
@@ -71,7 +67,7 @@ def create_alignment_feed(
         )
 
 
-def get_aligned_effects(alignment_scores_uuid):
+def get_aligned_effects(alignment_scores_uuid, n_nodes):
     """
     Create a sorted dictionary of IRIs and dot products for impacts/effects from the ontology that are positively associated with the top aligned personal
     value for users A and B (calculated based on comparison of their quiz results).
@@ -79,6 +75,7 @@ def get_aligned_effects(alignment_scores_uuid):
     Parameters
     ==========
     alignment_scores_uuid (UUID) - the uuid for the aligned scores for users a and b
+    n_nodes (int) - the number of effect nodes to output that score the highest. Default is 3 impacts.
 
     Returns
     ==========
@@ -127,7 +124,20 @@ def get_aligned_effects(alignment_scores_uuid):
         sorted(aligned_effects.items(), key=lambda x: x[1], reverse=True)
     )
 
-    return aligned_effects
+    aligned_effects_keys = list(aligned_effects.keys())
+    
+    aligned_effects_top_keys = []
+    if len(aligned_effects_keys) > n_nodes:
+        aligned_effects_top_keys = aligned_effects_keys[0:n_nodes]
+    else:
+        aligned_effects_top_keys = aligned_effects_keys
+
+    # TODO: delete this check after expansion of the ontology. This repeats the topics until the list is n long as needed.
+    aligned_effects_key_extra = list(aligned_effects.keys())[1]
+    while len(aligned_effects_keys) < n_nodes:
+        aligned_effects_top_keys.append(aligned_effects_key_extra)
+
+    return aligned_effects_top_keys
 
 
 def assign_alignment_iris(alignment_feed, field_type, iris):
