ede70a7c8cf7318fe29f43867b0149db823cb3bc
Cyprian Laskowski <cyp@cjvt.si>
2022-05-01 20:16:10 +0200

    CM-923: lint

--- app/feed/tests/test_process_alignment_feed.py
+++ app/feed/tests/test_process_alignment_feed.py
@@ -9,6 +9,7 @@ from app.feed.process_alignment_feed import (
     UNPOPULAR_SOLUTION_COUNT,
 )
 
+
 def test_find_alignment_solution_iris():
     nx = network_x_utils()
     solution_nodes = get_solution_nodes()
@@ -16,10 +17,21 @@ def test_find_alignment_solution_iris():
     for node in solution_nodes:
         nx.set_current_node(node)
         iri_label_map[nx.get_node_id()] = node["label"]
-    solution_iris = find_alignment_solution_iris(CONVERSATION_SOLUTION_NAME, POPULAR_SOLUTION_NAMES, POPULAR_SOLUTION_COUNT, UNPOPULAR_SOLUTION_COUNT)
+    solution_iris = find_alignment_solution_iris(
+        CONVERSATION_SOLUTION_NAME,
+        POPULAR_SOLUTION_NAMES,
+        POPULAR_SOLUTION_COUNT,
+        UNPOPULAR_SOLUTION_COUNT,
+    )
     expected_conversation_iri = solution_iris[0]
-    popular_iris = [iri for iri in solution_iris[1:] if iri_label_map[iri] in POPULAR_SOLUTION_NAMES]
-    unpopular_iris = [iri for iri in solution_iris[1:] if iri_label_map[iri] not in POPULAR_SOLUTION_NAMES]
-    assert(iri_label_map[expected_conversation_iri] == CONVERSATION_SOLUTION_NAME)
-    assert(len(popular_iris) == POPULAR_SOLUTION_COUNT)
-    assert(len(unpopular_iris) == UNPOPULAR_SOLUTION_COUNT)
+    popular_iris = [
+        iri for iri in solution_iris[1:] if iri_label_map[iri] in POPULAR_SOLUTION_NAMES
+    ]
+    unpopular_iris = [
+        iri
+        for iri in solution_iris[1:]
+        if iri_label_map[iri] not in POPULAR_SOLUTION_NAMES
+    ]
+    assert iri_label_map[expected_conversation_iri] == CONVERSATION_SOLUTION_NAME
+    assert len(popular_iris) == POPULAR_SOLUTION_COUNT
+    assert len(unpopular_iris) == UNPOPULAR_SOLUTION_COUNT
