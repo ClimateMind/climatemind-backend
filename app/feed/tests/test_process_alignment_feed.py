ea3bbdea9a8ab5e2c3f54fd84eb997e034652afe
Daniil Mashkin <daniil.mashkin@centralnic.com>
2022-05-05 00:01:00 +0100

    CM-923 app/feed/tests/test_process_alignment_feed.py comments added and little find_alignment_solution_iris refactoring

--- app/feed/tests/test_process_alignment_feed.py
+++ app/feed/tests/test_process_alignment_feed.py
@@ -1,7 +1,7 @@
 from app.network_x_tools.network_x_utils import network_x_utils
 
 from app.feed.process_alignment_feed import (
-    find_alignment_solution_iris,
+    get_default_solutions_iris,
     get_solution_nodes,
     CONVERSATION_SOLUTION_NAME,
     POPULAR_SOLUTION_NAMES,
@@ -10,28 +10,44 @@ from app.feed.process_alignment_feed import (
 )
 
 
-def test_find_alignment_solution_iris():
+def test_get_default_solutions_iris():
     nx = network_x_utils()
     solution_nodes = get_solution_nodes()
-    iri_label_map = {}
+    iri_name_map = {}
+
     for node in solution_nodes:
         nx.set_current_node(node)
-        iri_label_map[nx.get_node_id()] = node["label"]
-    solution_iris = find_alignment_solution_iris(
-        CONVERSATION_SOLUTION_NAME,
-        POPULAR_SOLUTION_NAMES,
-        POPULAR_SOLUTION_COUNT,
-        UNPOPULAR_SOLUTION_COUNT,
+        iri_name_map[nx.get_node_id()] = node["label"]
+
+    result_solution_iris = get_default_solutions_iris()
+    conversation_solution_count = 1
+    total_solutions_count = (
+        POPULAR_SOLUTION_COUNT + UNPOPULAR_SOLUTION_COUNT + conversation_solution_count
     )
-    expected_conversation_iri = solution_iris[0]
-    popular_iris = [
-        iri for iri in solution_iris[1:] if iri_label_map[iri] in POPULAR_SOLUTION_NAMES
+    assert (
+        len(result_solution_iris) == total_solutions_count
+    ), f"Total solutions cound should be {total_solutions_count}"
+
+    first_iri_in_result = result_solution_iris[0]
+    assert (
+        iri_name_map[first_iri_in_result] == CONVERSATION_SOLUTION_NAME
+    ), "First iri in result should be CONVERSATION iri"
+
+    result_without_conversation_iri = result_solution_iris[1:]
+    popular_iris_in_result = [
+        iri
+        for iri in result_without_conversation_iri
+        if iri_name_map[iri] in POPULAR_SOLUTION_NAMES
     ]
+    assert (
+        len(popular_iris_in_result) == POPULAR_SOLUTION_COUNT
+    ), "Result contain proper popular solution count"
+
     unpopular_iris = [
         iri
-        for iri in solution_iris[1:]
-        if iri_label_map[iri] not in POPULAR_SOLUTION_NAMES
+        for iri in result_without_conversation_iri
+        if iri_name_map[iri] not in POPULAR_SOLUTION_NAMES
     ]
-    assert iri_label_map[expected_conversation_iri] == CONVERSATION_SOLUTION_NAME
-    assert len(popular_iris) == POPULAR_SOLUTION_COUNT
-    assert len(unpopular_iris) == UNPOPULAR_SOLUTION_COUNT
+    assert (
+        len(unpopular_iris) == UNPOPULAR_SOLUTION_COUNT
+    ), "Result contain proper unpopular solution count"
