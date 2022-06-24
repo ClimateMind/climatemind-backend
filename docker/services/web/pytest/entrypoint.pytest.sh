2e0c4bdea3b6aed082e723d5da88c9f9b3aad56a
Kameron Rodrigues <kameronr@email.arizona.edu>
2022-06-24 14:43:18 -0700

    Separate Pytest from Docker-compose up
    Because database spins up upon docker-compose up

--- docker/services/web/pytest/entrypoint.pytest.sh
+++ docker/services/web/pytest/entrypoint.pytest.sh
@@ -3,8 +3,11 @@
 echo "sleeping entrypoint"
 sleep 120
 
+# python
+# flask run --host=0.0.0.0
+
 # FIXME: unable to run pytest in parallel with xdist plugin due to the error
 #  Transaction (Process ID 52) was deadlocked on lock resources with another process and has been chosen as the deadlock victim. Rerun the transaction. (1205) (SQLExecDirectW)')
 # pytest -n auto --cipdb
 
-pytest --cipdb
+# pytest --cipdb
