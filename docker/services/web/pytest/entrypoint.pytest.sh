81cb87c9a333ac3f416299d451ed36dd7a8faabf
danmash <daniil.mashkin@gmail.com>
2022-06-24 20:46:59 +0200

    pytest deadlock hotfix

--- docker/services/web/pytest/entrypoint.pytest.sh
+++ docker/services/web/pytest/entrypoint.pytest.sh
@@ -3,4 +3,8 @@
 echo "sleeping entrypoint"
 sleep 120
 
-pytest -n auto --cipdb
+# FIXME: unable to run pytest in parallel with xdist plugin due to the error
+#  Transaction (Process ID 52) was deadlocked on lock resources with another process and has been chosen as the deadlock victim. Rerun the transaction. (1205) (SQLExecDirectW)')
+# pytest -n auto --cipdb
+
+pytest --cipdb
