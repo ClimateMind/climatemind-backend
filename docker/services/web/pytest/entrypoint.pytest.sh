968387f11b3f4070656a4369e3f0205d289533d2
Kameron Rodrigues <kameronr@email.arizona.edu>
2022-06-24 15:43:25 -0700

    Keep web container on
    By having it run the web app

--- docker/services/web/pytest/entrypoint.pytest.sh
+++ docker/services/web/pytest/entrypoint.pytest.sh
@@ -4,7 +4,7 @@ echo "sleeping entrypoint"
 sleep 120
 
 # python
-# flask run --host=0.0.0.0
+flask run --host=0.0.0.0
 
 # FIXME: unable to run pytest in parallel with xdist plugin due to the error
 #  Transaction (Process ID 52) was deadlocked on lock resources with another process and has been chosen as the deadlock victim. Rerun the transaction. (1205) (SQLExecDirectW)')
