016b24067897da87368aa21369ff4c338aee75fc
Kameron Rodrigues <kameronr@email.arizona.edu>
2022-06-30 14:06:11 -0700

    Change to path variables instead of hard coding

--- docker/services/web/prod/entrypoint.prod.sh
+++ docker/services/web/prod/entrypoint.prod.sh
@@ -3,6 +3,8 @@
 echo "sleeping entrypoint"
 sleep 45
 
-flask db upgrade
+BASEDIR="$(dirname "${BASH_SOURCE[0]}")"
+
+flask db upgrade -d $BASEDIR/migrations
 
 gunicorn --bind 0.0.0.0:5000 climatemind:app
