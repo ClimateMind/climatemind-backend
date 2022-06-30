7a358a07dff9cf498db0860d02544bdb65349b70
danmash <danmash66@gmail.com>
2022-06-30 20:20:42 +0200

    drop_index outdated ix_users__email

---
+++ migrations/versions/202206301903-64616e69696c_drop_index_duplicate.py
@@ -0,0 +1,25 @@
+"""drop index duplicate
+
+Revision ID: 64616e69696c
+Revises: f3cbcfe42467
+Create Date: 2022-06-30 19:03:01.414667
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "64616e69696c"
+down_revision = "f3cbcfe42467"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_index("ix_users__email", table_name="users")
+
+
+def downgrade():
+    # this query could fail since there's no `email` field anymore
+    # op.create_index(None, "users", ["email"], unique=True)
+    pass
