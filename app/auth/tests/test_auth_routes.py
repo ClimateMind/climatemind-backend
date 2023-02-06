1ee19f0eb21dc489386d9aa0eda61f04ce4608c2
Cyprian Laskowski <cyplas@gmail.com>
2023-02-06 22:07:03 +0100

    #396: replaced login cypress test with pytest test (#484)

--- app/auth/tests/test_auth_routes.py
+++ app/auth/tests/test_auth_routes.py
@@ -53,6 +53,103 @@ def test_logout(m_unset_jwt_cookies, client):
     m_unset_jwt_cookies.assert_called_once()
 
 
+recaptcha_Token = "03AGdBq27Tmja4W082LAEVoYyuuALGQwMVxOuOGDduLCQSTWWFuTtc4hQsc-KUVhsJQlBzEjdtxTqs1kXHusJCk2husZjY44rA-opJLWOgJuVUIoGtXozHtYhtmR5DibuJ3idGLalZ00niqnaa0zHC73hWPzc1CtnUO258nZLh1uxePi7DI-afWQd6aa4-EuRcPabG_E500r9S4RReTg42WtP8SNrqEdFoG9UdPoIF2aGCArHD6GqhQzwOev8_jeKUzcxq_1wEvxiID2ow7rxK339PCeTgO9Zz9fPnhTZ6mKaa_tmL1bSQ2zvWvA0Z5An3YvMP3sureZVR_mhJP2r84sYw9WbuI6hRr1oUGtTGuACB-IBqqE5m-meetr870N2Gl-vp3veeEyo34HLj5iDOr6YwyIXWBKam7mDHfhjps1QeiN90291e6CxaFd-bOkeazZyu2_aEPblNwIiUBl0BobqJ2dT2HlxXCRma0QDuX4xLvwh8_ayrJGo9t6nRxQHghZ2ZEh450bM0bVFAqkIGAqYv_EvYj7_XgQ"  # this is the string from the cypress test I'm replacing ...
+recaptcha_Token = "abcdef"  # ... but any string works!
+
+
+@pytest.mark.integration
+@mock.patch("app.auth.routes.check_if_local")
+def test_successful_login(m_check_if_local, client):
+    m_check_if_local.return_value = True  # to test for recaptchaToken
+    registration = get_fake_registration_json()
+    client.post(url_for("auth.register"), json=registration).json["user"]
+
+    response = client.post(
+        url_for("auth.login"),
+        json={
+            "email": registration["email"],
+            "password": registration["password"],
+            "recaptchaToken": recaptcha_Token,
+        },
+    )
+    assert response.status_code == 200, "Successful login"
+
+    header_map = {name.lower(): value.lower() for (name, value) in response.headers}
+    assert header_map["content-type"] == "application/json", "content-type is json"
+    assert header_map["access-control-allow-origin"] == "http://0.0.0.0:3000"
+
+    assert isinstance(response.json, dict), "response is a dict"
+
+    assert "message" in response.json.keys(), "response has a message"
+    assert isinstance(response.json["message"], str), "response message is a string"
+
+    assert "access_token" in response.json.keys(), "response has an access_token"
+    assert isinstance(
+        response.json["access_token"], str
+    ), "response access_token is a string"
+
+    assert "user" in response.json.keys(), "response has a user"
+    assert isinstance(response.json["user"], dict), "response user is a dict"
+    assert isinstance(
+        response.json["user"]["first_name"], str
+    ), "response user first_name is a string"
+    assert isinstance(
+        response.json["user"]["last_name"], str
+    ), "response user last_name is a string"
+    assert isinstance(
+        response.json["user"]["email"], str
+    ), "response user email is a string"
+    assert isinstance(
+        response.json["user"]["user_uuid"], str
+    ), "response user uuid is a string"
+    print(response.json["user"]["quiz_id"])
+    assert isinstance(
+        response.json["user"]["quiz_id"], str
+    ), "response user quiz id {} is not a string".format(
+        response.json["user"]["quiz_id"]
+    )
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize(
+    "data",  # interpret None field as missing field, and no field as registration value
+    [
+        {"email": None, "password": None},
+        {"email": None},
+        {"password": None},
+        {"email": faker.email()},
+        {"password": faker.password()},
+    ],
+)
+def test_failed_login(data, client):
+    registration = get_fake_registration_json()
+    client.post(url_for("auth.register"), json=registration).json["user"]
+
+    status_code = 401
+    for key in ["email", "password"]:
+        if key not in data:
+            data[key] = registration[key]
+        elif data[key] is None:
+            status_code = 400
+    if status_code == 400:
+        message = "Email and password must be included in the request body."
+    elif status_code == 401:
+        message = "Wrong email or password. Try again."
+
+    response = client.post(
+        url_for("auth.login"),
+        json={
+            "email": data["email"],
+            "password": data["password"],
+        },
+    )
+
+    assert response.status_code == status_code, ("Failed login", response.status_code)
+    header_map = {name.lower(): value.lower() for (name, value) in response.headers}
+    assert header_map["content-type"] == "application/json"
+    assert response.json == {"error": message}, "Expected error message"
+
+
 def get_fake_registration_json():
     score = ScoresFactory()
     return {
