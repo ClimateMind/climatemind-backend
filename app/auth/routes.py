588bdcbfeb6d73e92f14671515005cacbf87d62e
Svenstar74 <sven.firmbach@gmx.de>
2023-08-06 20:17:49 +0200

    change spelling

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -76,7 +76,7 @@ def login():
     if not user or not user.check_password(password):
         raise UnauthorizedError(message="Wrong email or password. Try again.")
 
-    if not check_if_local() and not r.get("skip_captcha", None):
+    if not check_if_local() and not r.get("skipCaptcha", None):
         # Verify captcha with Google
         secret_key = os.environ.get("RECAPTCHA_SECRET_KEY")
 
