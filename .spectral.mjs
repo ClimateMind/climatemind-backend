0d46915c4445eb6ead9ccf32a308014fb6fb94e0
kameronr@email.arizona.edu <kameronr@email.arizona.edu>
2023-09-24 18:43:15 -0700

    added api documentation

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjExMTMzMA/spectral.js?branch_slug=develop&token=2948c464-0310-4d50-9655-54d07670ae90";
+export default { extends: ruleset };
