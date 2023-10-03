c2348b5c68124bd448f314f42dc5ebdcf3bec55d
Daniil Mashkin <dm@zint.io>
2023-10-03 13:04:45 +0100

    #499 migration black

--- migrations/versions/202310031147-ec4000ea90cb_499_explicit_ondelete.py
+++ migrations/versions/202310031147-ec4000ea90cb_499_explicit_ondelete.py
@@ -10,31 +10,61 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ec4000ea90cb'
-down_revision = '7d5d345fee36'
+revision = "ec4000ea90cb"
+down_revision = "7d5d345fee36"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.drop_constraint("fk_conversations__sender_user_uuid__users", "conversations", type_="foreignkey")
-    op.create_foreign_key(None, "conversations", "users", ["sender_user_uuid"], ["user_uuid"], ondelete="SET NULL")
+    op.drop_constraint(
+        "fk_conversations__sender_user_uuid__users", "conversations", type_="foreignkey"
+    )
+    op.create_foreign_key(
+        None,
+        "conversations",
+        "users",
+        ["sender_user_uuid"],
+        ["user_uuid"],
+        ondelete="SET NULL",
+    )
 
-    op.drop_constraint("fk_password_reset_links__user_uuid__users", "password_reset_links", type_="foreignkey")
-    op.create_foreign_key(None, "password_reset_links", "users", ["user_uuid"], ["user_uuid"],  ondelete="cascade")
+    op.drop_constraint(
+        "fk_password_reset_links__user_uuid__users",
+        "password_reset_links",
+        type_="foreignkey",
+    )
+    op.create_foreign_key(
+        None,
+        "password_reset_links",
+        "users",
+        ["user_uuid"],
+        ["user_uuid"],
+        ondelete="cascade",
+    )
 
     op.drop_constraint("fk_sessions__user_uuid__users", "sessions", type_="foreignkey")
-    op.create_foreign_key(None, "sessions", "users", ["user_uuid"], ["user_uuid"], ondelete="SET NULL")
+    op.create_foreign_key(
+        None, "sessions", "users", ["user_uuid"], ["user_uuid"], ondelete="SET NULL"
+    )
 
 
 def downgrade():
-    op.drop_constraint("fk_conversations__sender_user_uuid__users", "conversations", type_="foreignkey")
-    op.create_foreign_key(None, "conversations", "users", ["sender_user_uuid"], ["user_uuid"])
+    op.drop_constraint(
+        "fk_conversations__sender_user_uuid__users", "conversations", type_="foreignkey"
+    )
+    op.create_foreign_key(
+        None, "conversations", "users", ["sender_user_uuid"], ["user_uuid"]
+    )
 
-    op.drop_constraint("fk_password_reset_links__user_uuid__users", "password_reset_links", type_="foreignkey")
-    op.create_foreign_key(None, "password_reset_links", "users", ["user_uuid"], ["user_uuid"])
+    op.drop_constraint(
+        "fk_password_reset_links__user_uuid__users",
+        "password_reset_links",
+        type_="foreignkey",
+    )
+    op.create_foreign_key(
+        None, "password_reset_links", "users", ["user_uuid"], ["user_uuid"]
+    )
 
     op.drop_constraint("fk_sessions__user_uuid__users", "sessions", type_="foreignkey")
     op.create_foreign_key(None, "sessions", "users", ["user_uuid"], ["user_uuid"])
-
-
