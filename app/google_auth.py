8cca5abb881a83e924eefef28ba29685866845d7
Svenstar74 <sven.firmbach@gmx.de>
2024-07-08 16:57:57 +0200

    change backend url

--- app/google_auth.py
+++ app/google_auth.py
@@ -5,6 +5,12 @@ oauth = OAuth()
 
 
 def init_google_auth(app):
+    backend_base_url = 'http://localhost:5000'
+    if os.getenv('SENTRY_ENVIRONMENT') == 'staging':
+        backend_base_url = 'https://app-backend-test-001.azurewebsites.net'
+    elif os.getenv('SENTRY_ENVIRONMENT') == 'production':
+        backend_base_url = 'https://app-backend-prod-001.azurewebsites.net'
+
     oauth.init_app(app)
     google = oauth.register(
         'google',
@@ -13,7 +19,7 @@ def init_google_auth(app):
         client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
         authorize_url='https://accounts.google.com/o/oauth2/auth',
         access_token_url='https://www.googleapis.com/oauth2/v4/token',
-        redirect_uri='https://app-backend-test-001.azurewebsites.net/login/callback',
+        redirect_uri= backend_base_url + '/login/callback',
         client_kwargs={'scope': 'openid profile email'},
     )
     return google
